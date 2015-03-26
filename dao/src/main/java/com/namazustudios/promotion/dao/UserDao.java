9d865a5c8ecb00a5282d540f6d9ebe66846c4a3f
Patrick Twohig <patrick@namazustudios.com>
2015-03-26 02:02:38 -0700

    - Added dao and mongo-dao projects

---
+++ dao/src/main/java/com/namazustudios/promotion/dao/UserDao.java
@@ -0,0 +1,7 @@
+package com.namazustudios.promotion.dao;
+
+/**
+ * Created by patricktwohig on 3/26/15.
+ */
+public interface UserDao {
+}
---
+++ mongo-dao/src/main/java/com/namazustudios/promotion/dao/mongo/MongoUserDao.java
@@ -0,0 +1,7 @@
+package com.namazustudios.promotion.dao.mongo;
+
+/**
+ * Created by patricktwohig on 3/26/15.
+ */
+public class MongoUserDao {
+}
