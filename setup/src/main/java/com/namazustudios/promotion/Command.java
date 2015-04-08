a9709c0ee98e9756dc4924fd91a06592a812ed44
Patrick Twohig <patrick@namazustudios.com>
2015-04-08 10:58:27 -0700

    - Refactored the setup program to be more modular

---
+++ setup/src/main/java/com/namazustudios/promotion/Command.java
@@ -0,0 +1,10 @@
+package com.namazustudios.promotion;
+
+/**
+ * Created by patricktwohig on 4/8/15.
+ */
+public interface Command  {
+
+    public void run(final String[] args) throws Exception;
+
+}
--- setup/src/main/java/com/namazustudios/promotion/Setup.java
+++ setup/src/main/java/com/namazustudios/promotion/Setup.java
@@ -1,20 +1,14 @@
 package com.namazustudios.promotion;
 
 import com.google.common.base.Strings;
+import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import com.namazustudios.promotion.dao.UserDao;
 import com.namazustudios.promotion.dao.mongo.guice.MongoDaoModule;
-import com.namazustudios.promotion.exception.DuplicateException;
-import com.namazustudios.promotion.model.User;
 import com.namazustudios.promotion.rest.guice.ConfigurationModule;
-import joptsimple.OptionException;
-import joptsimple.OptionParser;
-import joptsimple.OptionSet;
-import joptsimple.OptionSpec;
 
-import javax.inject.Inject;
 import java.io.Console;
+import java.util.Arrays;
 
 /**
  * Hello world!
@@ -22,100 +16,47 @@ import java.io.Console;
  */
 public class Setup {
 
-    // Command line options
-
-    private final OptionParser optionParser = new OptionParser();
-
-    private OptionSpec<String> rootUsernameOptionSpec;
-
-    private OptionSpec<String> rootPasswordOptionSpec;
-
-    private OptionSpec<String> rootEmailOptionSpec;
-
-    // The data models to use
-
-    private User rootUser = new User();
-
-    private String rootPassword;
-
-    @Inject
-    private UserDao userDao;
-
-    public Setup() {
-
-        rootUsernameOptionSpec = optionParser.accepts("root-user", "Root Username")
-            .withOptionalArg()
-            .ofType(String.class);
-
-        rootPasswordOptionSpec = optionParser.accepts("root-password", "Root Password")
-            .withOptionalArg()
-            .ofType(String.class);
-
-        rootEmailOptionSpec = optionParser.accepts("root-email", "Root Email Address")
-            .withOptionalArg()
-            .ofType(String.class);
-
-    }
-
-    public User getRootUser() {
-        return rootUser;
-    }
-
-    public void setRootUser(User rootUser) {
-        this.rootUser = rootUser;
-    }
-
-    public String getRootPassword() {
-        return rootPassword;
-    }
-
-    public void setRootPassword(String rootPassword) {
-        this.rootPassword = rootPassword;
-    }
 
     public void run(final String args[]) throws Exception {
 
-        final Injector injector = Guice.createInjector(
-                new ConfigurationModule(),
-                new MongoDaoModule());
+        if (args.length < 1) {
+            System.err.print("Please specify commandType.");
+        }
 
-        injector.injectMembers(this);
+        final Class<? extends Command> commandType;
 
         try {
-            final OptionSet optionSet = optionParser.parse(args);
-            readOptions(optionSet);
-        } catch (OptionException ex) {
-            optionParser.printHelpOn(System.out);
+            commandType = SupportedCommand.getCommandForName(args[0]);
+        } catch (IllegalArgumentException ex) {
+            System.out.printf("Unknown commandType %s.  Supported commands are:\n", args[0]);
+
+            for (SupportedCommand supportedCommand : SupportedCommand.values()) {
+                System.out.println("    " + supportedCommand.commandName);
+            }
+
             return;
         }
 
-        writeRootUserToDatabase();
+        final Injector injector = Guice.createInjector(
+                new ConfigurationModule(),
+                new MongoDaoModule(),
+                new AbstractModule() {
+
+                    @Override
+                    protected void configure() {
+                        binder().bind(Setup.class).toInstance(Setup.this);
+                    }
+
+                });
+
+        final Command command = injector.getInstance(commandType);
+        final String[] subArgs = Arrays.copyOfRange(args, 1, args.length);
+
+        command.run(subArgs);
 
     }
 
-    private void readOptions(final OptionSet optionSet) {
-
-        getRootUser().setName(optionSet.valueOf(rootUsernameOptionSpec));
-        getRootUser().setEmail(optionSet.valueOf(rootEmailOptionSpec));
-        setRootPassword(optionSet.valueOf(rootPasswordOptionSpec));
-
-        if (Strings.isNullOrEmpty(getRootUser().getName())) {
-            rootUser.setName(read("Please enter root username: "));
-        }
-
-        if (Strings.isNullOrEmpty(rootUser.getEmail())) {
-            rootUser.setEmail(read("Please enter root email: "));
-        }
-
-        if (Strings.isNullOrEmpty(rootPassword)) {
-            rootPassword = reads("Please enter root password: ");
-        }
-
-        rootUser.setLevel(User.Level.SUPERUSER);
-
-    }
-
-    private String read(final String fmt, Object... args) {
+    public String read(final String fmt, Object... args) {
 
         final Console console = System.console();
 
@@ -133,7 +74,7 @@ public class Setup {
 
     }
 
-    private String reads(final String fmt, Object... args) {
+    public String reads(final String fmt, Object... args) {
 
         final Console console = System.console();
 
@@ -151,27 +92,34 @@ public class Setup {
 
     }
 
-    private void writeRootUserToDatabase() {
-
-        // Creates or updates the user
-
-        try {
-            userDao.createUser(rootUser);
-        } catch (DuplicateException ex) {
-            userDao.updateUser(rootUser);
-        }
-
-        userDao.updateUserPassword(rootUser.getName(), rootPassword);
-
-        // Validate that we can get both the username and password
-        userDao.validateUserPassword(rootUser.getName(), rootPassword);
-        userDao.validateUserPassword(rootUser.getEmail(), rootPassword);
-
-    }
-
     public static void main( String[] args ) throws Exception {
         final Setup setup = new Setup();
         setup.run(args);
+    }
+
+    private enum SupportedCommand {
+
+        ADD_USER("add-user", AddUser.class);
+
+        public final String commandName;
+        public final Class<? extends Command> commandType;
+
+        private SupportedCommand(final String commandName, final Class<? extends  Command> command) {
+            this.commandName = commandName;
+            this.commandType = command;
+        }
+
+        public static Class<? extends Command> getCommandForName(final String name) {
+
+            for (SupportedCommand supportedCommand : values()) {
+                if (supportedCommand.commandName.equals(name)) {
+                    return supportedCommand.commandType;
+                }
+            }
+
+            throw new IllegalArgumentException("Unknown command: " + name);
+        }
+
     }
 
 }
