f4d8dc6d210977c28e1cfb17339227fd7d7e1ad5
Patrick Twohig <patrick@namazustudios.com>
2015-04-30 17:03:01 -0700

    Separated web-ui and web-ui-war

---
+++ web-ui-war/src/main/java/com/namazustudios/promotion/App.java
@@ -0,0 +1,13 @@
+package com.namazustudios.promotion;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
