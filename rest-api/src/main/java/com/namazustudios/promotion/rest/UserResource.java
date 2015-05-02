cbdd8c745832365febdf08e9086b804a0b29c0b4
Patrick Twohig <patrick@namazustudios.com>
2015-05-01 23:13:55 -0700

    Revert "Added Dispatch rest client"
    This reverts commit 6a25fc00cc6e8f3cda46a68032df639b4707f06e.

--- rest-api/src/main/java/com/namazustudios/promotion/rest/UserResource.java
+++ rest-api/src/main/java/com/namazustudios/promotion/rest/UserResource.java
@@ -53,12 +53,6 @@ public class UserResource {
         return userService.getUser(name);
     }
 
-    @GET
-    @Path("me")
-    public User getUser() {
-        return userService.getCurrentUser();
-    }
-
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     public User createUser(final User user) {
