ce663f0448d4a9a46eb063b5fa0b7dc3de97e271
Patrick Twohig <patrick@namazustudios.com>
2015-05-12 23:38:00 -0700

    Added pom (which was missing for some reason) Added subbed-out functionality for the DefaultDocumentGnerator

--- lucene-fts/src/main/java/com/namazustudios/socialengine/DefaultDocumentGenerator.java
+++ lucene-fts/src/main/java/com/namazustudios/socialengine/DefaultDocumentGenerator.java
@@ -4,6 +4,9 @@ import com.namazustudios.socialengine.annotation.SearchableDocument;
 import org.apache.lucene.document.Document;
 
 /**
+ * The default instance of the {@link DocumentGenerator}, this is a simple
+ * implementation that just reads the annotations and converts.
+ *
  * Created by patricktwohig on 5/12/15.
  */
 public class DefaultDocumentGenerator implements DocumentGenerator {
