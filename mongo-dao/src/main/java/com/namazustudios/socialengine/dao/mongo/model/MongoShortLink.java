ba267d6388da2d7cd1d05db4d0e0f7e1c1fbca13
Patrick Twohig <patrick@namazustudios.com>
2015-05-13 21:23:18 -0700

    renamed lucene-fts -> lucene-pojo

--- mongo-dao/src/main/java/com/namazustudios/socialengine/dao/mongo/model/MongoShortLink.java
+++ mongo-dao/src/main/java/com/namazustudios/socialengine/dao/mongo/model/MongoShortLink.java
@@ -1,5 +1,6 @@
 package com.namazustudios.socialengine.dao.mongo.model;
 
+import com.namazustudios.socialengine.fts.annotation.SearchableDocument;
 import org.mongodb.morphia.Key;
 import org.mongodb.morphia.annotations.Entity;
 import org.mongodb.morphia.annotations.Id;
