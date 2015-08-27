2bfd933aa0d4c5215cd60eb28c5e667382f2386c
Patrick Twohig <patrick@namazustudios.com>
2015-08-27 01:11:04 -0700

    Added the IoC resolver.

---
+++ rt-server-lua/src/main/java/com/namazustudios/socialengine/rt/lua/IocResolver.java
@@ -0,0 +1,31 @@
+package com.namazustudios.socialengine.rt.lua;
+
+/**
+ * Used by Lua resources to access member that would otherwise be provided using
+ * the javax.inject annotations {@link javax.inject.Inject} and {@link javax.inject.Named}.
+ *
+ * Created by patricktwohig on 8/27/15.
+ */
+public interface IocResolver {
+
+    /**
+     * Gets the type as if it was annotated with the {@link javax.inject.Inject} annotation.
+     *
+     * @param tClass
+     * @param <T>
+     * @return
+     */
+    <T> T inject(Class<T> tClass);
+
+    /**
+     * Gets the type as if it was annotated with the {@link javax.inject.Inject} and {@link javax.inject.Named}
+     * annotation.
+     *
+     * @param tClass
+     * @param named
+     * @param <T>
+     * @return
+     */
+    <T> T inject(Class<T> tClass, String named);
+
+}
