22308d4a0ec13882b2c11513a862b583140af209
Patrick Twohig <patrick@namazustudios.com>
2015-09-03 00:54:47 -0700

    Added MinaServerModule. Added Main example class Addded more bindings to the ServerModule

---
+++ rt-server-guice-example/src/main/java/com/namazustudios/socialengine/rt/guice/example/Main.java
@@ -0,0 +1,36 @@
+package com.namazustudios.socialengine.rt.guice.example;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.namazustudios.socialengine.rt.Constants;
+import com.namazustudios.socialengine.rt.guice.SimpleServerModule;
+import com.namazustudios.socialengine.rt.lua.guice.ClasspathScanningLuaResourceModule;
+import com.namazustudios.socialengine.rt.mina.guice.MinaServerModule;
+import org.apache.mina.transport.socket.SocketAcceptor;
+
+import java.net.InetSocketAddress;
+
+/**
+ * Created by patricktwohig on 9/1/15.
+ */
+public class Main {
+
+    public static void main(final String[] args) throws Exception {
+        final Injector injector = Guice.createInjector(
+            new SimpleServerModule(),
+            new MinaServerModule(),
+            new ClasspathScanningLuaResourceModule() {
+                @Override
+                protected void configure() {
+                    scanForEdgeResources("edge");
+                    scanForInternalResources("internal");
+                }
+            }
+        );
+
+        final SocketAcceptor socketAcceptor = injector.getInstance(SocketAcceptor.class);
+        socketAcceptor.bind(new InetSocketAddress(Constants.DEFAULT_PORT));
+
+    }
+
+}
---
+++ rt-server-mina/src/main/java/com/namazustudios/socialengine/rt/mina/guice/MinaServerModule.java
@@ -0,0 +1,44 @@
+package com.namazustudios.socialengine.rt.mina.guice;
+
+import com.google.inject.Provider;
+import com.namazustudios.socialengine.rt.mina.BSONProtocolDecoder;
+import com.namazustudios.socialengine.rt.mina.BSONProtocolEncoder;
+import org.apache.mina.core.service.IoAcceptor;
+import org.apache.mina.filter.codec.ProtocolDecoder;
+import org.apache.mina.filter.codec.ProtocolEncoder;
+import org.apache.mina.filter.logging.LoggingFilter;
+import org.apache.mina.guice.MinaModule;
+import org.apache.mina.guice.filter.InjectProtocolCodecFilter;
+import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
+
+
+/**
+ * Created by patricktwohig on 9/3/15.
+ */
+public class MinaServerModule extends MinaModule {
+
+    @Override
+    protected void configureMINA() {
+
+        bind(IoAcceptor.class).toProvider(new Provider<NioSocketAcceptor>() {
+
+            @Override
+            public NioSocketAcceptor get() {
+                final NioSocketAcceptor nioSocketAcceptor = new NioSocketAcceptor();
+                nioSocketAcceptor.getSessionConfig().setTcpNoDelay(true);
+                return nioSocketAcceptor;
+            }
+
+        });
+
+        bindFilter().named("logging").atAndOfFilterChain().to(LoggingFilter.class);
+        bindFilter().named("codec").atAndOfFilterChain().to(InjectProtocolCodecFilter.class);
+
+        bindFilterChainBuilder();
+        bindProtocolCodecFactory();
+        bind(ProtocolEncoder.class).to(BSONProtocolEncoder.class);
+        bind(ProtocolDecoder.class).to(BSONProtocolDecoder.class);
+
+    }
+
+}
