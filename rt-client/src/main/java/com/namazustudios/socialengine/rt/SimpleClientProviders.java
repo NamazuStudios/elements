7154986fcc921cabbab1b8f5e6885204cf98401a
Patrick Twohig <patrick@namazustudios.com>
2015-09-11 17:16:05 -0700

    Added simple client bindings.

---
+++ rt-client/src/main/java/com/namazustudios/socialengine/rt/SimpleClientProviders.java
@@ -0,0 +1,64 @@
+package com.namazustudios.socialengine.rt;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Provider;
+
+/**
+ * Implements various {@link Provider} instances for {@link SimpleClient}
+ *
+ * Created by patricktwohig on 9/11/15.
+ */
+public class SimpleClientProviders {
+
+    private SimpleClientProviders() {}
+
+    public static Provider<SimpleClient> getInternalClientProvider() {
+        return new Provider<SimpleClient>() {
+
+            @Inject
+            @Named(Client.TRANSPORT_INTERNAL)
+            Provider<ClientRequestDispatcher> clientRequestDispatcherProvider;
+
+            @Override
+            public SimpleClient get() {
+                final ClientRequestDispatcher clientRequestDispatcher = clientRequestDispatcherProvider.get();
+                return new SimpleClient(clientRequestDispatcher);
+            }
+
+        };
+    }
+
+    public static Provider<SimpleClient> getReliableTransportClientProvider() {
+        return new Provider<SimpleClient>() {
+
+            @Inject
+            @Named(Client.TRANSPORT_RELIABLE)
+            Provider<ClientRequestDispatcher> clientRequestDispatcherProvider;
+
+            @Override
+            public SimpleClient get() {
+                final ClientRequestDispatcher clientRequestDispatcher = clientRequestDispatcherProvider.get();
+                return new SimpleClient(clientRequestDispatcher);
+            }
+
+        };
+    }
+
+    public static Provider<SimpleClient> getBestEffortTransportClientProvider() {
+        return new Provider<SimpleClient>() {
+
+            @Inject
+            @Named(Client.TRANSPORT_BEST_EFFORT)
+            Provider<ClientRequestDispatcher> clientRequestDispatcherProvider;
+
+            @Override
+            public SimpleClient get() {
+                final ClientRequestDispatcher clientRequestDispatcher = clientRequestDispatcherProvider.get();
+                return new SimpleClient(clientRequestDispatcher);
+            }
+
+        };
+    }
+
+}
--- rt-client/src/main/java/com/namazustudios/socialengine/rt/guice/SimpleClientModule.java
+++ rt-client/src/main/java/com/namazustudios/socialengine/rt/guice/SimpleClientModule.java
@@ -1,6 +1,12 @@
 package com.namazustudios.socialengine.rt.guice;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Scopes;
+import com.google.inject.name.Names;
+import com.google.inject.util.Providers;
+import com.namazustudios.socialengine.rt.Client;
+import com.namazustudios.socialengine.rt.SimpleClient;
+import com.namazustudios.socialengine.rt.SimpleClientProviders;
 
 /**
  * Created by patricktwohig on 9/11/15.
@@ -9,8 +15,58 @@ public class SimpleClientModule extends AbstractModule {
 
     @Override
     protected final void configure() {
-
+        bindClients();
     }
 
+    protected void bindClients() {
+
+        // Binds the SimpleClient in singletons for each annotated type.  Each
+        // is bound in Singleton so there will only be one instance of SimpleCLient
+        // for each of the types.
+
+        binder().bind(SimpleClient.class)
+                .annotatedWith(Names.named(Client.TRANSPORT_INTERNAL))
+                .toProvider(Providers.guicify(SimpleClientProviders.getInternalClientProvider()))
+                .in(Scopes.SINGLETON);
+
+        binder().bind(SimpleClient.class)
+                .annotatedWith(Names.named(Client.TRANSPORT_BEST_EFFORT))
+                .toProvider(Providers.guicify(SimpleClientProviders.getBestEffortTransportClientProvider()))
+                .in(Scopes.SINGLETON);
+
+        binder().bind(SimpleClient.class)
+                .annotatedWith(Names.named(Client.TRANSPORT_RELIABLE))
+                .toProvider(Providers.guicify(SimpleClientProviders.getReliableTransportClientProvider()))
+                .in(Scopes.SINGLETON);
+
+        // Binds the Client interface to the simple client
+
+        binder().bind(Client.class)
+                .annotatedWith(Names.named(Client.TRANSPORT_INTERNAL))
+                .to(SimpleClient.class);
+
+        binder().bind(Client.class)
+                .annotatedWith(Names.named(Client.TRANSPORT_BEST_EFFORT))
+                .to(SimpleClient.class);
+
+        binder().bind(Client.class)
+                .annotatedWith(Names.named(Client.TRANSPORT_RELIABLE))
+                .to(SimpleClient.class);
+
+        // Binds the network operations interface to the simple client
+
+        binder().bind(Client.NetworkOperations.class)
+                .annotatedWith(Names.named(Client.TRANSPORT_INTERNAL))
+                .to(SimpleClient.class);
+
+        binder().bind(Client.NetworkOperations.class)
+                .annotatedWith(Names.named(Client.TRANSPORT_BEST_EFFORT))
+                .to(SimpleClient.class);
+
+        binder().bind(Client.NetworkOperations.class)
+                .annotatedWith(Names.named(Client.TRANSPORT_RELIABLE))
+                .to(SimpleClient.class);
+
+    }
 
 }
