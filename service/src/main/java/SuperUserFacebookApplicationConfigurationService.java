de5cf71b053ca85feb9527622d78f93400a421fb
Patrick Twohig <patrick@namazustudios.com>
2017-06-14 18:09:21 -0700

    PTWI-182: Added implementation of the SuperUserFacebookApplicationConfigurationService

--- service/src/main/java/SuperUserFacebookApplicationConfigurationService.java
+++ service/src/main/java/SuperUserFacebookApplicationConfigurationService.java
@@ -1,27 +1,43 @@
+import com.namazustudios.socialengine.dao.FacebookApplicationConfigurationDao;
 import com.namazustudios.socialengine.model.application.FacebookApplicationConfiguration;
 import com.namazustudios.socialengine.service.FacebookApplicationConfigurationService;
+
+import javax.inject.Inject;
 
 /**
  * Created by patricktwohig on 6/14/17.
  */
 public class SuperUserFacebookApplicationConfigurationService implements FacebookApplicationConfigurationService {
+
+    private FacebookApplicationConfigurationDao facebookApplicationConfigurationDao;
+
     @Override
     public void deleteApplicationConfiguration(String applicationNameOrId, String applicationConfigurationNameOrId) {
-
+        getFacebookApplicationConfigurationDao().softDeleteApplicationProfile(applicationNameOrId, applicationConfigurationNameOrId);
     }
 
     @Override
     public FacebookApplicationConfiguration getApplicationConfiguration(String applicationNameOrId, String applicationConfigurationNameOrId) {
-        return null;
+        return getFacebookApplicationConfigurationDao().getIosApplicationProfile(applicationNameOrId, applicationConfigurationNameOrId);
     }
 
     @Override
     public FacebookApplicationConfiguration createApplicationConfiguration(String applicationNameOrId, FacebookApplicationConfiguration facebookApplicationConfiguration) {
-        return null;
+        return getFacebookApplicationConfigurationDao().createOrUpdateInactiveApplicationProfile(applicationNameOrId, facebookApplicationConfiguration);
     }
 
     @Override
     public FacebookApplicationConfiguration updateApplicationConfiguration(String applicationNameOrId, String applicationConfigurationNameOrId, FacebookApplicationConfiguration facebookApplicationConfiguration) {
-        return null;
+        return getFacebookApplicationConfigurationDao().updateApplicationProfile(applicationNameOrId, applicationConfigurationNameOrId, facebookApplicationConfiguration);
     }
+
+    public FacebookApplicationConfigurationDao getFacebookApplicationConfigurationDao() {
+        return facebookApplicationConfigurationDao;
+    }
+
+    @Inject
+    public void setFacebookApplicationConfigurationDao(FacebookApplicationConfigurationDao facebookApplicationConfigurationDao) {
+        this.facebookApplicationConfigurationDao = facebookApplicationConfigurationDao;
+    }
+
 }
