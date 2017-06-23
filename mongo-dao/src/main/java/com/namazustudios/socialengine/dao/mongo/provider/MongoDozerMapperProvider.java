c12254b548aa2fba1bade3c4fd51b6d57720d192
Patrick Twohig <patrick@namazustudios.com>
2017-06-23 14:56:12 -0700

    PTWI-182: Updated dozer mapper to properly map users

--- mongo-dao/src/main/java/com/namazustudios/socialengine/dao/mongo/provider/MongoDozerMapperProvider.java
+++ mongo-dao/src/main/java/com/namazustudios/socialengine/dao/mongo/provider/MongoDozerMapperProvider.java
@@ -2,7 +2,6 @@ package com.namazustudios.socialengine.dao.mongo.provider;
 
 import com.namazustudios.socialengine.dao.mongo.converter.ObjectIdConverter;
 import com.namazustudios.socialengine.dao.mongo.model.*;
-import com.namazustudios.socialengine.model.User;
 import com.namazustudios.socialengine.model.application.*;
 import org.dozer.DozerBeanMapper;
 import org.dozer.Mapper;
@@ -42,9 +41,6 @@ public class MongoDozerMapperProvider implements Provider<Mapper> {
                 mapping(FacebookApplicationConfiguration.class, MongoFacebookApplicationConfiguration.class)
                     .fields("id", "objectId", customConverter(ObjectIdConverter.class))
                     .fields("applicationId","uniqueIdentifier");
-
-                mapping(User.class, MongoUser.class)
-                    .fields("id", "objectId", customConverter(ObjectIdConverter.class));
 
             }
         };
