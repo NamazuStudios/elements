b82cc248b66d103e843bca69f064aac42cc9f67b
Patrick Twohig <patrick@namazustudios.com>
2017-10-19 01:35:45 -0700

    SOC-47: Added skeleton request code

---
+++ rt-server-lua/src/main/resources/namazu/request.lua
@@ -0,0 +1,27 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: patricktwohig
+-- Date: 10/19/17
+-- Time: 1:32 AM
+-- To change this template use File | Settings | File Templates.
+--
+
+local table = requre "table"
+local request = {}
+
+function request.formulate(request, payload, headers, parameters)
+    -- TODO Write this code.
+    return nil
+end
+
+function request.unpack_header(request, header_name)
+    -- TODO Write this code.
+    return nil
+end
+
+function request.unpack_parameter(reqest, parameter_name)
+    -- TODO Write this code.
+    return nil
+end
+
+return request
