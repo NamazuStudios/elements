4d9fdf7c80b87ee4b91c244affd4c27d741c7ee0
Patrick Twohig <patrick@namazustudios.com>
2017-11-21 22:02:07 -0800

    SOC-39: Added HttpHeader (to replace Property in Header definitions)

---
+++ rt-common/src/main/java/com/namazustudios/socialengine/rt/manifest/http/HttpHeader.java
@@ -0,0 +1,15 @@
+package com.namazustudios.socialengine.rt.manifest.http;
+
+import com.namazustudios.socialengine.rt.manifest.model.Type;
+
+public class HttpHeader {
+
+    private String name;
+
+    private String description;
+
+    private Type type;
+
+    private String securityScheme;
+
+}
