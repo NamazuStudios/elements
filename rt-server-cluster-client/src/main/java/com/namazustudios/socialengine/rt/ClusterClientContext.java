e23d7e32f48367dfe5192333402be5ae6d1d747a
Patrick Twohig <patrick@namazustudios.com>
2017-12-01 18:32:23 -0800

    SOC-43: Added skeleton project SOC-43: Separated some of the server logic into a new type

---
+++ rt-server-cluster-client/src/main/java/com/namazustudios/socialengine/rt/ClusterClientContext.java
@@ -0,0 +1,32 @@
+package com.namazustudios.socialengine.rt;
+
+public class ClusterClientContext implements Context {
+
+    // TODO Implement this class (Skeleton Only At the Moment)
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+
+    @Override
+    public ResourceContext getResourceContext() {
+        return null;
+    }
+
+    @Override
+    public SchedulerContext getSchedulerContext() {
+        return null;
+    }
+
+    @Override
+    public IndexContext getIndexContext() {
+        return null;
+    }
+
+}
