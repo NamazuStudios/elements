e4a4b6221875482ba1fb1563921aa9f0f29e1377
Patrick Twohig <patrick@namazustudios.com>
2018-02-24 00:11:38 -0800

    SOC-92: Created FacebookSessionRequest and UsernamePasswordSessionRequest classee

---
+++ common/src/main/java/com/namazustudios/socialengine/model/session/FacebookSessionRequest.java
@@ -0,0 +1,68 @@
+package com.namazustudios.socialengine.model.session;
+
+import io.swagger.annotations.ApiParam;
+
+public class FacebookSessionRequest {
+
+    private String applicationNameOrId;
+
+    private String applicationConfigurationNameOrId;
+
+    private String facebookOAuthAccessToken;
+
+    public String getApplicationNameOrId() {
+        return applicationNameOrId;
+    }
+
+    public void setApplicationNameOrId(String applicationNameOrId) {
+        this.applicationNameOrId = applicationNameOrId;
+    }
+
+    public String getApplicationConfigurationNameOrId() {
+        return applicationConfigurationNameOrId;
+    }
+
+    public void setApplicationConfigurationNameOrId(String applicationConfigurationNameOrId) {
+        this.applicationConfigurationNameOrId = applicationConfigurationNameOrId;
+    }
+
+    public String getFacebookOAuthAccessToken() {
+        return facebookOAuthAccessToken;
+    }
+
+    public void setFacebookOAuthAccessToken(String facebookOAuthAccessToken) {
+        this.facebookOAuthAccessToken = facebookOAuthAccessToken;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof FacebookSessionRequest)) return false;
+
+        FacebookSessionRequest that = (FacebookSessionRequest) o;
+
+        if (getApplicationNameOrId() != null ? !getApplicationNameOrId().equals(that.getApplicationNameOrId()) : that.getApplicationNameOrId() != null)
+            return false;
+        if (getApplicationConfigurationNameOrId() != null ? !getApplicationConfigurationNameOrId().equals(that.getApplicationConfigurationNameOrId()) : that.getApplicationConfigurationNameOrId() != null)
+            return false;
+        return getFacebookOAuthAccessToken() != null ? getFacebookOAuthAccessToken().equals(that.getFacebookOAuthAccessToken()) : that.getFacebookOAuthAccessToken() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getApplicationNameOrId() != null ? getApplicationNameOrId().hashCode() : 0;
+        result = 31 * result + (getApplicationConfigurationNameOrId() != null ? getApplicationConfigurationNameOrId().hashCode() : 0);
+        result = 31 * result + (getFacebookOAuthAccessToken() != null ? getFacebookOAuthAccessToken().hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "FacebookSessionRequest{" +
+                "applicationNameOrId='" + applicationNameOrId + '\'' +
+                ", applicationConfigurationNameOrId='" + applicationConfigurationNameOrId + '\'' +
+                ", facebookOAuthAccessToken='" + facebookOAuthAccessToken + '\'' +
+                '}';
+    }
+
+}
---
+++ common/src/main/java/com/namazustudios/socialengine/model/session/UsernamePasswordRequest.java
@@ -0,0 +1,51 @@
+package com.namazustudios.socialengine.model.session;
+
+public class UsernamePasswordRequest {
+
+    private String userId;
+
+    private String password;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof UsernamePasswordRequest)) return false;
+
+        UsernamePasswordRequest that = (UsernamePasswordRequest) o;
+
+        if (getUserId() != null ? !getUserId().equals(that.getUserId()) : that.getUserId() != null) return false;
+        return getPassword() != null ? getPassword().equals(that.getPassword()) : that.getPassword() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getUserId() != null ? getUserId().hashCode() : 0;
+        result = 31 * result + (getPassword() != null ? getPassword().hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "UsernamePasswordRequest{" +
+                "userId='" + userId + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+}
