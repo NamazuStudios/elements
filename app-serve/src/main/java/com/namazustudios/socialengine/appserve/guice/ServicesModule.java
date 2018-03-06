2048fad561b25b1756b9f49c73df020118e6530c
Patrick Twohig <patrick@namazustudios.com>
2018-03-06 11:07:35 -0800

    SOC-128: Added missing binding

--- app-serve/src/main/java/com/namazustudios/socialengine/appserve/guice/ServicesModule.java
+++ app-serve/src/main/java/com/namazustudios/socialengine/appserve/guice/ServicesModule.java
@@ -1,9 +1,12 @@
 package com.namazustudios.socialengine.appserve.guice;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.servlet.ServletScopes;
 import com.namazustudios.socialengine.service.*;
 import com.namazustudios.socialengine.service.application.SuperUserApplicationService;
+import com.namazustudios.socialengine.service.auth.AnonSessionService;
 import com.namazustudios.socialengine.service.auth.AnonUsernamePasswordAuthService;
+import com.namazustudios.socialengine.service.auth.SessionServiceProvider;
 import com.namazustudios.socialengine.service.auth.StandardFacebookAuthService;
 
 public class ServicesModule extends AbstractModule {
@@ -14,6 +17,7 @@ public class ServicesModule extends AbstractModule {
         bind(ApplicationService.class).to(SuperUserApplicationService.class);
         bind(FacebookAuthService.class).to(StandardFacebookAuthService.class);
         bind(VersionService.class).to(BuildPropertiesVersionService.class).asEagerSingleton();
+        bind(SessionService.class).to(AnonSessionService.class);
     }
 
 }
