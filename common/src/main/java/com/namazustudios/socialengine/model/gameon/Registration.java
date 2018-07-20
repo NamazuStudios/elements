4dfe668d68abeb8bfc356a5cd4d03237c9746f5c
Patrick Twohig <patrick@namazustudios.com>
2018-07-19 18:48:22 -0700

    SOC-190: Added registration class

---
+++ common/src/main/java/com/namazustudios/socialengine/model/gameon/Registration.java
@@ -0,0 +1,62 @@
+package com.namazustudios.socialengine.model.gameon;
+
+import com.namazustudios.socialengine.model.profile.Profile;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@ApiModel(description = "Associates an Amazon GameOn registration with a particular profile.  There may exist only " +
+                        "one Registration ")
+public class Registration implements Serializable {
+
+    @ApiModelProperty("The unique ID of this registration.")
+    private String id;
+
+    @ApiModelProperty("The profile assocaited with this GameOn registration.")
+    private Profile profile;
+
+    @ApiModelProperty("The Amazon-issued Player Token")
+    private String playerToken;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Profile getProfile() {
+        return profile;
+    }
+
+    public void setProfile(Profile profile) {
+        this.profile = profile;
+    }
+
+    public String getPlayerToken() {
+        return playerToken;
+    }
+
+    public void setPlayerToken(String playerToken) {
+        this.playerToken = playerToken;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (!(object instanceof Registration)) return false;
+        Registration that = (Registration) object;
+        return Objects.equals(getId(), that.getId()) &&
+                Objects.equals(getProfile(), that.getProfile()) &&
+                Objects.equals(getPlayerToken(), that.getPlayerToken());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getProfile(), getPlayerToken());
+    }
+
+}
