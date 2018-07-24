92173418190f6d0ac5f838286ab3502782d15f13
Patrick Twohig <patrick@namazustudios.com>
2018-07-23 17:11:39 -0700

    SOC-190: Pointed direct at game on API

--- service/src/main/java/com/namazustudios/socialengine/service/gameon/client/Constants.java
+++ service/src/main/java/com/namazustudios/socialengine/service/gameon/client/Constants.java
@@ -18,7 +18,6 @@ public interface Constants {
     /**
      * The base API for Amazon GameOn
      */
-//    String BASE_API = "https://api.amazongameon.com";
-    String BASE_API = "https://localhost:8280";
+    String BASE_API = "https://api.amazongameon.com";
 
 }
