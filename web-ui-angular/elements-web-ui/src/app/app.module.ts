22bef9d698df796d677684ae693d1d4d1ce2e38b
David Brooks <djbrooks@inlandsoftware.com>
2018-10-18 16:59:37 -0700

    Ongoing development

--- web-ui-angular/elements-web-ui/src/app/app.module.ts
+++ web-ui-angular/elements-web-ui/src/app/app.module.ts
@@ -1,11 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { TopMenuComponent } from './top-menu/top-menu.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { FlexLayoutModule } from "@angular/flex-layout";
+
+const appRoutes: Routes = [
+  // { path: 'users', component: UserListComponent },
+  // { path: 'users/:id', component: UserDetailComponent },
+  // { path: 'applications', component: ApplicationListComponent },
+  // { path: 'applications/:id', component: ApplicationDetailComponent },
+  // { path: 'applications/:applicationId/configurations/:id', component: ApplicationConfigurationDetailComponent },
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
@@ -13,6 +24,10 @@ import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, Mat
     TopMenuComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     BrowserAnimationsModule,
     LayoutModule,
@@ -20,7 +35,8 @@ import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, Mat
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
