bbf6fd11bf28271f41b6508476f3670a341a0802
David Brooks <djbrooks@inlandsoftware.com>
2018-10-18 14:39:10 -0700

    Actually commit the files this time

---
+++ web-ui-angular/elements-web-ui/src/app/top-menu/top-menu.component.spec.ts
@@ -0,0 +1,24 @@
+
+import { fakeAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TopMenuComponent } from './top-menu.component';
+
+describe('TopMenuComponent', () => {
+  let component: TopMenuComponent;
+  let fixture: ComponentFixture<TopMenuComponent>;
+
+  beforeEach(fakeAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TopMenuComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TopMenuComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should compile', () => {
+    expect(component).toBeTruthy();
+  });
+});
