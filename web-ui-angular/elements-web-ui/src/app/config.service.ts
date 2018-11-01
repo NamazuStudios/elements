c92d4ad8f07743cfd8d8b600cc634e65e7a1fb42
David Brooks <djbrooks@inlandsoftware.com>
2018-11-01 08:09:13 -0700

    Final (?!) fixes

--- web-ui-angular/elements-web-ui/src/app/config.service.ts
+++ web-ui-angular/elements-web-ui/src/app/config.service.ts
@@ -10,7 +10,7 @@ export class ConfigService {
 
   load() {
 
-    return this.http.get('/assets/config.json')
+    return this.http.get('./assets/config.json')
       .toPromise()
       .then(data => {
         this.appConfig = data;
