caf4f9bd1e80d0162f23021a5cbb83a00aa9d877
David Brooks <djbrooks@inlandsoftware.com>
2018-11-14 20:31:03 -0800

    Continued development on inventory API (incomplete)

--- service/src/main/java/com/namazustudios/socialengine/service/SimpleInventoryItemService.java
+++ service/src/main/java/com/namazustudios/socialengine/service/SimpleInventoryItemService.java
@@ -7,11 +7,22 @@ import java.util.Set;
 
 public interface SimpleInventoryItemService {
 
-    InventoryItem getInventoryItem(String inventoryItemId);
+    /**
+     * Creates, or updates, a new {@link Score}.  If the profile submitted with the the {@link Score} matches an
+     * existing {@link Leaderboard} and {@link Profile}, then the existing record will be updated.
+     *
+     *
+     * @param leaderboardNameOrId the value of {@link Leaderboard#getId()} or {@link Leaderboard#getName()}
+     * @param score the {@link Score}
+     * @return the {@link Score} as it was written to the database
+     */
+    InventoryItem getInventoryItem(String itemId);
 
     Pagination<InventoryItem> getInventoryItems(int offset, int count, String query);
 
-    InventoryItem updateInventoryItem(InventoryItem item);
+    InventoryItem adjustInventoryItemQuantity(String itemId, Integer quantityDelta);
 
     InventoryItem createInventoryItem(InventoryItem item);
+
+    void deleteInventoryItem(String itemId);
 }
