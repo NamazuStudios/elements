07235a1aeee516e166bb5b912e465123e2b497ce
Dan <dmetzler@ucsd.edu>
2019-02-08 14:19:52 -0800

    SOC-214: implemnt item model interface

---
+++ web-ui-angular/elements-web-ui/src/app/models/item-view-model.ts
@@ -0,0 +1,10 @@
+import {Item} from '../api/models/item';
+
+export class ItemViewModel implements Item {
+  name: string;
+  description: string;
+  id: string;
+  tags: Array<string>;
+  displayName: string;
+  metadata: {[key: string]: string};
+}
