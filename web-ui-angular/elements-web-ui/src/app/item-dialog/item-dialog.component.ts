b7ac89dbe6229c8efda6c6349f0677e803564155
Dan <dmetzler@ucsd.edu>
2019-02-09 23:31:35 -0800

    SOC-214: Added tags to UI

--- web-ui-angular/elements-web-ui/src/app/item-dialog/item-dialog.component.ts
+++ web-ui-angular/elements-web-ui/src/app/item-dialog/item-dialog.component.ts
@@ -1,6 +1,7 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {FormBuilder, Validators} from '@angular/forms';
-import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+import {MAT_DIALOG_DATA, MatChipInputEvent, MatDialogRef} from '@angular/material';
+import {ENTER, COMMA, TAB} from '@angular/cdk/keycodes';
 
 @Component({
   selector: 'app-item-dialog',
@@ -8,6 +9,10 @@ import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
   styleUrls: ['./item-dialog.component.css']
 })
 export class ItemDialogComponent implements OnInit {
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  readonly separatorKeyCodes: number[] = [ENTER, TAB, COMMA];
 
   constructor(public dialogRef: MatDialogRef<ItemDialogComponent>,
               @Inject(MAT_DIALOG_DATA) public data: any,
@@ -17,7 +22,22 @@ export class ItemDialogComponent implements OnInit {
     name: [ this.data.item.name, [Validators.required, Validators.pattern('^[a-zA-Z0-9]+$') ]],
     displayName: [ this.data.item.displayName, [Validators.required]],
     description: [ this.data.item.description ],
+    tags: [ this.data.item.tags ]
   });
+
+  addTag(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    if ((value || '').trim()) {
+      this.data.item.tags.push(value);
+    }
+
+    if (input) {
+      input.value = '';
+    }
+    console.log(this.data.item.tags);
+  }
 
   ngOnInit() {
   }
