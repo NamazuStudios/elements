b8da56c40c8718b3e8367f5f3e606646f3536d1b
Dan <dmetzler@ucsd.edu>
2019-02-25 13:19:53 -0800

    SOC-225: building out view of existing data

--- web-ui-angular/elements-web-ui/src/app/mission-dialog/mission-steps-card/mission-steps-card.component.ts
+++ web-ui-angular/elements-web-ui/src/app/mission-dialog/mission-steps-card/mission-steps-card.component.ts
@@ -1,6 +1,5 @@
-import {AfterContentInit, Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Mission} from '../../api/models/mission';
-import {MissionStepViewModel} from '../../models/mission-step-view-model';
 import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
@@ -15,7 +14,10 @@ export class MissionStepsCardComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder) { }
 
-  public stepForm = this.formBuilder.group({});
+  public stepForm = this.formBuilder.group({
+    newRewardItem: [],
+    newRewardCt: []
+  });
 
   drop(event: CdkDragDrop<string[]>) {
     console.log(this.mission.steps);
@@ -23,17 +25,6 @@ export class MissionStepsCardComponent implements OnInit {
   }
 
   ngOnInit() {
-    if (!this.mission.steps) {
-      this.mission.steps = [];
-    }
-    for (let i = 0; i < 5; i++) {
-      this.mission.steps.push(new MissionStepViewModel());
-      const newStep = this.mission.steps[i];
-      newStep.count = 1;
-      newStep.description = 'Description of step ' + i;
-      newStep.displayName = 'Step ' + i;
-    }
-
     for (let i = 0; i < this.mission.steps.length; i++) {
       this.stepForm.addControl('displayName' + i, new FormControl('', Validators.required));
       this.stepForm.addControl('description' + i, new FormControl('', Validators.required));
