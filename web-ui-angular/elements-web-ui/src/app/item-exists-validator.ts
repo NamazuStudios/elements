5fde005118ada5006ebb3f17ebd082fc1a2e6840
Dan <dmetzler@ucsd.edu>
2019-03-05 12:09:29 -0800

    SOC-225: fixed async custom validator for reward items

--- web-ui-angular/elements-web-ui/src/app/item-exists-validator.ts
+++ web-ui-angular/elements-web-ui/src/app/item-exists-validator.ts
@@ -1,13 +1,24 @@
 import {AbstractControl, AsyncValidator, ValidationErrors} from '@angular/forms';
 import {ItemsService} from './api/services/items.service';
-import {Observable} from 'rxjs';
-import {map as __map} from 'rxjs/operators';
+import {Observable, of as __of} from 'rxjs';
+import {catchError as __catchError, map as __map} from 'rxjs/operators';
+import {HttpErrorResponse} from '@angular/common/http';
 
 export class ItemExistsValidator implements AsyncValidator {
-  constructor(private itemsService: ItemsService) {}
+  constructor(private itemsService: ItemsService) {
+    this.validate = this.validate.bind(this);
+  }
+
+  private handleError(error: HttpErrorResponse): Observable<ValidationErrors> {
+    return __of({itemNameNotFound: {value: ''}});
+  }
 
   validate(control: AbstractControl): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
-    return this.itemsService.getItemByIdentifier(control.value)
-      .pipe(__map(item => item ? null : {itemNameNotFound: {value: control.value}}))
+    return this.itemsService.getItemByIdentifier(control.value).pipe(
+        __map(item => {
+          console.log(item);
+          return item ? null : {itemNameNotFound: {value: control.value}}; }),
+        __catchError(this.handleError)
+      );
   }
 }
