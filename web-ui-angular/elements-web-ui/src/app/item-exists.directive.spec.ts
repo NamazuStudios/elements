8ff75fdfc7478330a4eac5bb8aee1688d5b00422
Dan <dmetzler@ucsd.edu>
2019-03-05 15:30:45 -0800

    SOC-225: removed outdated validator

--- web-ui-angular/elements-web-ui/src/app/item-exists.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ItemExistsDirective } from './item-exists.directive';
-
-describe('ItemExistsDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ItemExistsDirective();
-    expect(directive).toBeTruthy();
-  });
-});
