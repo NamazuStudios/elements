73201bf088e395c92a3b39d7ac08de528d458d75
Dan <dmetzler@ucsd.edu>
2019-03-30 14:45:25 -0700

    SOC-294: populate bundle editor fields

--- web-ui-angular/elements-web-ui/src/app/product-bundle-editor/product-bundle-editor.component.ts
+++ web-ui-angular/elements-web-ui/src/app/product-bundle-editor/product-bundle-editor.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+import {FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-product-bundle-editor',
@@ -6,8 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-bundle-editor.component.css']
 })
 export class ProductBundleEditorComponent implements OnInit {
+  productBundleForm = this.formBuilder.group({
+    productId: [this.data.productBundle.productId, [Validators.required]],
+    displayName: [this.data.productBundle.displayName, [Validators.required]],
+    description: [this.data.productBundle.description, [Validators.required]]
+  });
 
-  constructor() { }
+  constructor(public dialogRef: MatDialogRef<ProductBundleEditorComponent>,
+              @Inject(MAT_DIALOG_DATA) public data: any,
+              private formBuilder: FormBuilder) { }
 
   ngOnInit() {
   }
