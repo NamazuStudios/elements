d65c6330d9641bc27cba33ef70632975179b651a
Dan <dmetzler@ucsd.edu>
2019-03-30 14:20:52 -0700

    SOC-294: open product bundle editor on edit click

--- web-ui-angular/elements-web-ui/src/app/product-bundle-list/product-bundle-list.component.ts
+++ web-ui-angular/elements-web-ui/src/app/product-bundle-list/product-bundle-list.component.ts
@@ -1,6 +1,10 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {ApplicationConfigurationsDataSource} from '../application-configuration.datasource';
 import {ProductBundle} from '../api/models/product-bundle';
+import {filter} from 'rxjs/operators';
+import {ConfirmationDialogService} from '../confirmation-dialog/confirmation-dialog.service';
+import {MatDialog} from '@angular/material';
+import {ProductBundleEditorComponent} from '../product-bundle-editor/product-bundle-editor.component';
 
 @Component({
   selector: 'app-product-bundle-list',
@@ -13,14 +17,29 @@ export class ProductBundleListComponent implements OnInit {
   dataSource: ApplicationConfigurationsDataSource;
   displayedColumns = ["select", "id", "displayName", "display", "actions"];
 
-  constructor() { }
+  constructor(private dialogService: ConfirmationDialogService,
+              public dialog: MatDialog) { }
 
   editProductBundle(productBundle: ProductBundle) {
+    this.showDialog(false, ProductBundleEditorComponent, productBundle, res => {
 
+    });
   }
 
   deleteProductBundle(productBundle: ProductBundle) {
 
+  }
+
+  showDialog(isNew: boolean, dialog: any, productBundle: ProductBundle, next) {
+    const dialogRef = this.dialog.open(dialog, {
+      width: '900px',
+      data: { isNew: isNew, productBundle: productBundle }
+    });
+
+    dialogRef
+      .afterClosed()
+      .pipe(filter(r => r))
+      .subscribe(next);
   }
 
   ngOnInit() {
