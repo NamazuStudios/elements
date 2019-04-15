dde220742de63c8c7186a8f631d17f446ca0d5c2
Keith Hudnall <keith@namazustudios.com>
2019-04-15 15:41:57 -0700

    SOC-236 Changed code to errorCode to match the GameOn defined model

--- service/src/main/java/com/namazustudios/socialengine/service/gameon/client/model/ErrorResponse.java
+++ service/src/main/java/com/namazustudios/socialengine/service/gameon/client/model/ErrorResponse.java
@@ -2,9 +2,17 @@ package com.namazustudios.socialengine.service.gameon.client.model;
 
 public class ErrorResponse {
 
-    private String message;
+    int errorCode;
+    String message;
+    String request_id;
 
-    private Error error;
+    public int getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(int code) {
+        this.errorCode = code;
+    }
 
     public String getMessage() {
         return message;
@@ -14,41 +22,11 @@ public class ErrorResponse {
         this.message = message;
     }
 
-    public Error getError() {
-        return error;
+    public String getRequest_id() {
+        return request_id;
     }
 
-    public void setError(Error error) {
-        this.error = error;
-    }
-
-    public static class Error {
-        int errorCode;
-        String message;
-        String request_id;
-
-        public int getErrorCode() {
-            return errorCode;
-        }
-
-        public void setErrorCode(int code) {
-            this.errorCode = code;
-        }
-
-        public String getMessage() {
-            return message;
-        }
-
-        public void setMessage(String message) {
-            this.message = message;
-        }
-
-        public String getRequest_id() {
-            return request_id;
-        }
-
-        public void setRequest_id(String request_id) {
-            this.request_id = request_id;
-        }
+    public void setRequest_id(String request_id) {
+        this.request_id = request_id;
     }
 }
