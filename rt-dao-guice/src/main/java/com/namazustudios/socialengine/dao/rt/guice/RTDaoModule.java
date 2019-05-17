a94a3fb694db79eecfc4e82c5c14c7c6c88dbd52
Patrick Twohig <patrick@namazustudios.com>
2019-05-17 00:29:23 -0700

    SOC-328: Added missing bindings.

--- rt-dao-guice/src/main/java/com/namazustudios/socialengine/dao/rt/guice/RTDaoModule.java
+++ rt-dao-guice/src/main/java/com/namazustudios/socialengine/dao/rt/guice/RTDaoModule.java
@@ -1,6 +1,5 @@
 package com.namazustudios.socialengine.dao.rt.guice;
 
-import com.google.inject.AbstractModule;
 import com.google.inject.PrivateModule;
 import com.google.inject.TypeLiteral;
 import com.namazustudios.socialengine.dao.ContextFactory;
@@ -9,17 +8,10 @@ import com.namazustudios.socialengine.dao.rt.DefaultContextFactory;
 import com.namazustudios.socialengine.dao.rt.RTManifestDao;
 import com.namazustudios.socialengine.guice.ZContextModule;
 import com.namazustudios.socialengine.remote.jeromq.JeroMQConnectionMultiplexer;
-import com.namazustudios.socialengine.remote.jeromq.JeroMQRemoteInvoker;
 import com.namazustudios.socialengine.rt.ConnectionMultiplexer;
 import com.namazustudios.socialengine.rt.Context;
-import com.namazustudios.socialengine.rt.remote.RemoteInvoker;
-import org.zeromq.ZContext;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.ThreadPoolExecutor;
 import java.util.function.Function;
-
-import static com.google.inject.name.Names.named;
 
 /**
  * Created by patricktwohig on 8/22/17.
@@ -29,8 +21,10 @@ public class RTDaoModule extends PrivateModule {
     @Override
     protected void configure() {
 
-        expose(RTManifestDao.class);
-        expose(RTContextProvider.class);
+        expose(ManifestDao.class);
+        expose(ContextFactory.class);
+        expose(ConnectionMultiplexer.class);
+        expose(new TypeLiteral<Function<String, Context>>(){});
 
         install(new ZContextModule());
 
