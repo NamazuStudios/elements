502cfe27a25d7f96926cfb099d44012f844d5760
Patrick Twohig <patrick@namazustudios.com>
2019-08-01 16:50:19 -0700

    SOC-20: Completed Broadcast routing strategy test

--- rt-common/src/test/java/com/namazustudios/socialengine/rt/TestBroadcastRoutingStrategy.java
+++ rt-common/src/test/java/com/namazustudios/socialengine/rt/TestBroadcastRoutingStrategy.java
@@ -8,11 +8,13 @@ import org.testng.annotations.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 import java.util.function.Consumer;
 
 import static java.util.Arrays.asList;
-import static java.util.Collections.emptyList;
-import static java.util.Collections.unmodifiableList;
+import static java.util.Collections.*;
 import static org.mockito.Mockito.*;
 import static org.testng.Assert.*;
 
@@ -81,14 +83,21 @@ public class TestBroadcastRoutingStrategy extends BaseRoutingStrategyTest {
 
     }
 
-    @Test
+    @Test(expectedExceptions = {BullshitException.class})
     public void testInvokeSyncException() throws Exception {
 
-        final List<Object> address = spy(emptyList());
+        final List<Object> address = emptyList();
         final List<Consumer<InvocationResult>> asyncConsumers = new ArrayList<>();
         final InvocationErrorConsumer invocationErrorConsumer = mock(InvocationErrorConsumer.class);
         final Invocation invocation = spy(Invocation.class);
 
+        final RemoteInvoker mockRemoteInvoker = mock(RemoteInvoker.class);
+
+        when(getRemoteInvokerRegistry()
+            .getAllRemoteInvokers(eq(getDefaultApplicationUuid())))
+            .thenReturn(singletonList(mockRemoteInvoker));
+
+        when(mockRemoteInvoker.invokeSync(any(), any(), any())).thenThrow(new BullshitException());
         getRoutingStrategy().invokeSync(address, invocation, asyncConsumers, invocationErrorConsumer);
 
     }
@@ -96,10 +105,128 @@ public class TestBroadcastRoutingStrategy extends BaseRoutingStrategyTest {
     @Test
     public void testInvokeAsync() {
 
+        final List<Object> address = emptyList();
+
+        final InvocationErrorConsumer invocationErrorConsumer = mock(InvocationErrorConsumer.class);
+        final Invocation invocation = spy(Invocation.class);
+
+        final List<Consumer<InvocationResult>> asyncConsumers = unmodifiableList(asList(
+                (Consumer<InvocationResult>)mock(Consumer.class),
+                (Consumer<InvocationResult>)mock(Consumer.class)
+        ));
+
+        final List<RemoteInvoker> mockRemoteInvokers = unmodifiableList(asList(
+                mock(RemoteInvoker.class),
+                mock(RemoteInvoker.class),
+                mock(RemoteInvoker.class),
+                mock(RemoteInvoker.class),
+                mock(RemoteInvoker.class)
+        ));
+
+        final List<InvocationResult> invocationResultList = new ArrayList<>();
+
+        for(final RemoteInvoker ri : mockRemoteInvokers) {
+            when(ri.invokeAsync(eq(invocation), any(), any())).thenAnswer(i -> {
+                final List<Consumer<InvocationResult>> arg1 = i.getArgument(1);
+                arg1.forEach(c -> {
+                    final InvocationResult ir = new InvocationResult();
+                    c.accept(ir);
+                    invocationResultList.add(ir);
+                });
+                return null;
+            });
+        }
+
+        when(getRemoteInvokerRegistry()
+                .getAllRemoteInvokers(eq(getDefaultApplicationUuid())))
+                .thenReturn(mockRemoteInvokers);
+
+        final Object result = getRoutingStrategy().invokeAsync(
+                address,
+                invocation,
+                asyncConsumers,
+                invocationErrorConsumer);
+
+        assertNull(result, "Expected null return.");
+        verify(getRemoteInvokerRegistry(), times(1)).getAllRemoteInvokers(eq(getDefaultApplicationUuid()));
+
+        for (final RemoteInvoker ri : mockRemoteInvokers) {
+            verify(ri, times(1)).invokeAsync(
+                    eq(invocation),
+                    any(),
+                    any()
+            );
+        }
+
+        assertEquals(invocationResultList.size(), mockRemoteInvokers.size() * asyncConsumers.size());
+        invocationResultList.forEach(r -> assertNotNull(r));
+        invocationResultList.forEach(r -> assertNull(r.getResult()));
+
     }
 
     @Test
-    public void testInvokeFuture() {
+    public void testInvokeFuture() throws ExecutionException, InterruptedException {
+        final List<Object> address = emptyList();
+
+        final InvocationErrorConsumer invocationErrorConsumer = mock(InvocationErrorConsumer.class);
+        final Invocation invocation = spy(Invocation.class);
+
+        final List<Consumer<InvocationResult>> asyncConsumers = unmodifiableList(asList(
+                (Consumer<InvocationResult>)mock(Consumer.class),
+                (Consumer<InvocationResult>)mock(Consumer.class)
+        ));
+
+        final List<RemoteInvoker> mockRemoteInvokers = unmodifiableList(asList(
+                mock(RemoteInvoker.class),
+                mock(RemoteInvoker.class),
+                mock(RemoteInvoker.class),
+                mock(RemoteInvoker.class),
+                mock(RemoteInvoker.class)
+        ));
+
+        final List<InvocationResult> invocationResultList = new ArrayList<>();
+
+        for(final RemoteInvoker ri : mockRemoteInvokers) {
+            when(ri.invokeCompletionStage(eq(invocation), any(), any())).thenAnswer(i -> {
+                final List<Consumer<InvocationResult>> arg1 = i.getArgument(1);
+                arg1.forEach(c -> {
+                    final InvocationResult ir = new InvocationResult();
+                    c.accept(ir);
+                    invocationResultList.add(ir);
+                });
+
+                final CompletableFuture<Object> future = new CompletableFuture<>();
+                future.complete(null);
+                return future;
+
+            });
+        }
+
+        when(getRemoteInvokerRegistry()
+                .getAllRemoteInvokers(eq(getDefaultApplicationUuid())))
+                .thenReturn(mockRemoteInvokers);
+
+        final Future<Object> result = getRoutingStrategy().invokeFuture(
+                address,
+                invocation,
+                asyncConsumers,
+                invocationErrorConsumer);
+
+        assertNotNull(result, "Expected null return.");
+        verify(getRemoteInvokerRegistry(), times(1)).getAllRemoteInvokers(eq(getDefaultApplicationUuid()));
+
+        for (final RemoteInvoker ri : mockRemoteInvokers) {
+            verify(ri, times(1)).invokeCompletionStage(
+                    eq(invocation),
+                    any(),
+                    any()
+            );
+        }
+
+        assertEquals(invocationResultList.size(), mockRemoteInvokers.size() * asyncConsumers.size());
+        invocationResultList.forEach(r -> assertNotNull(r));
+        invocationResultList.forEach(r -> assertNull(r.getResult()));
+        assertNull(result.get());
 
     }
 
