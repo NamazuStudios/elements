06045573342f9ebc7386529fdc8b16cc597aad96
Patrick Twohig <patrick@namazustudios.com>
2020-08-18 12:05:18 -0700

    SOC-367:J Fixed compile error

--- mongo-dao/src/main/java/com/namazustudios/socialengine/dao/mongo/MongoAppleSignInUserDao.java
+++ mongo-dao/src/main/java/com/namazustudios/socialengine/dao/mongo/MongoAppleSignInUserDao.java
@@ -1,9 +1,22 @@
 package com.namazustudios.socialengine.dao.mongo;
 
 import com.namazustudios.socialengine.dao.AppleSignInUserDao;
+import com.namazustudios.socialengine.model.user.User;
 
 public class MongoAppleSignInUserDao implements AppleSignInUserDao {
 
     // TODO Implement this DAO
+
+    @Override
+    public User connectActiveAppleUserIfNecessary(User user) {
+        // TODO Implement Fully
+        return null;
+    }
+
+    @Override
+    public User createReactivateOrUpdateUser(User user) {
+        // TODO Implement Fully
+        return null;
+    }
 
 }
