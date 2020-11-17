0e18c5278072c00e6788175fa4e7d378a9dfc7a4
Garrett McSpadden <EmissaryEntertainment@aol.com>
2020-11-17 10:13:12 -0800

    SOC-20 Disabled app-serve-testkit unit tests. SOC-20 Added mockito to rest-api-test and added a new binding for InstanceConnectionService to the test module. SOC-20 removed unnecessary binding from RTDaoModule.java

--- rt-dao-guice/src/main/java/com/namazustudios/socialengine/dao/rt/guice/RTDaoModule.java
+++ rt-dao-guice/src/main/java/com/namazustudios/socialengine/dao/rt/guice/RTDaoModule.java
@@ -29,7 +29,6 @@ public class RTDaoModule extends PrivateModule {
         expose(new TypeLiteral<Function<String, Context>>(){});
 
         bind(ManifestDao.class).to(RTManifestDao.class).asEagerSingleton();
-        bind(ZContext.class).asEagerSingleton();
         bind(ContextFactory.class).to(DefaultContextFactory.class).asEagerSingleton();
         bind(new TypeLiteral<Function<String, Context>>(){}).toProvider(RTContextProvider.class);
 
