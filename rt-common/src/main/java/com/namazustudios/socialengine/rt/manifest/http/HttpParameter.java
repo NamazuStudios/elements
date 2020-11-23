9a5ff2ff391cd563760799aa9f55ceb35c7804e8
Robert Cornwall <>
2020-11-23 12:52:06 -0800

    SOC-398: added HttpParameter as a class

---
+++ rt-common/src/main/java/com/namazustudios/socialengine/rt/manifest/http/HttpParameter.java
@@ -0,0 +1,10 @@
+package com.namazustudios.socialengine.rt.manifest.http;
+
+import com.namazustudios.socialengine.rt.manifest.model.Type;
+
+public class HttpParameter {
+
+        private String name;
+
+        private Type type;
+}
