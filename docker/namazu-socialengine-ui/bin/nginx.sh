a2db7f0033969e296ed98971e753819b76f766ea
Patrick Twohig <patrick@namazustudios.com>
2021-05-24 19:52:00 -0700

    SOC-20: Added update to the nginx.sh launch script

--- docker/namazu-socialengine-ui/bin/nginx.sh
+++ docker/namazu-socialengine-ui/bin/nginx.sh
@@ -1,22 +1,47 @@
 #!/usr/bin/env bash
 
 # Converts the dot-environment variable syntax to the ENVIRONMENT_VARIABLE convention. This honors only
-# those environment variables which start with com.namazustudios. This is useful to prevent
+# those environment variables which start with com.namazustudios
+
+index_html="/usr/share/nginx/html/index.html"
+default_conf="/etc/nginx/conf.d/default.conf"
+config_json="/usr/share/nginx/html/assets/config.json"
 
 while IFS='=' read -r name value ; do
   if [[ $name == 'com.namazustudios'* ]]; then
     remapped="$(echo "$name" | tr . _)"
     export "$remapped"="$value"
-    echo "$remapped"="$value"
+    echo "$name -> $remapped = $value"
   fi
 done < <(env)
+
+echo "Remapped Environment Variables. Current environment: "
+echo ""
+env
 
 # Just-in-time, we write the files to the appropriate location on disk where nginx will serve the assets
 # in the UI properly as well as configure the port and server names.
 
-envsubst < "$ELEMENTS_CONF/index.html.template" > /usr/share/nginx/html/index.html
-envsubst < "$ELEMENTS_CONF/default.conf.template" > /etc/nginx/conf.d/default.conf
-envsubst < "$ELEMENTS_CONF/config.json.template" > /usr/share/nginx/html/assets/config.json
+echo "Generated '${index_html}'"
+echo BOF
+envsubst < "$ELEMENTS_CONF/index.html.template"
+echo EOF
+echo
 
-# Actually runs NGINX
+echo "Generated '${default_conf}'"
+echo BOF
+envsubst < "$ELEMENTS_CONF/default.conf.template"
+echo EOF
+echo
+
+echo "Generated '${config_json}'"
+echo BOF
+envsubst < "$ELEMENTS_CONF/config.json.template"
+echo EOF
+echo
+
+envsubst < "$ELEMENTS_CONF/index.html.template" > "${index_html}"
+envsubst < "$ELEMENTS_CONF/default.conf.template" > "${default_conf}"
+envsubst < "$ELEMENTS_CONF/config.json.template" > "${config_json}"
+
 nginx -g "daemon off;"
