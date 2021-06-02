4c19471aeedfe19bb2498205bbd85f84202ead14
Patrick Twohig <patrick@namazustudios.com>
2021-06-02 08:42:33 -0700

    SOC-20: Made script responsible for assigning default values

--- docker/namazu-socialengine-ui/bin/nginx.sh
+++ docker/namazu-socialengine-ui/bin/nginx.sh
@@ -3,6 +3,13 @@
 # Converts the dot-environment variable syntax to the ENVIRONMENT_VARIABLE convention. This honors only
 # those environment variables which start with com.namazustudios
 
+# Default values for the environment variables.
+# shellcheck disable=SC2034
+export com_namazustudios_socialengine_http_port=8080
+export com_namazustudios_socialengine_http_path_prefix web-ui
+export com_namazustudios_socialengine_dns_name localhost:8080
+export com_namazustudios_socialengine_api_url http://localhost:8080/api/rest
+
 index_html="/usr/share/nginx/html/index.html"
 default_conf="/etc/nginx/conf.d/default.conf"
 config_json="/usr/share/nginx/html/assets/config.json"
