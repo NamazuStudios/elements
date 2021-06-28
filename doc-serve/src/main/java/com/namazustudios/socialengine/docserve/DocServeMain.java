33fb40a89a91545ffa2f3faa7a0183a6076ebe8f
Patrick Twohig <patrick@namazustudios.com>
2021-06-28 14:17:05 -0700

    SOC-403: Added root-package aliases for the various subservices.

---
+++ doc-serve/src/main/java/com/namazustudios/socialengine/docserve/DocServeMain.java
@@ -0,0 +1,9 @@
+package com.namazustudios.socialengine.docserve;
+
+public class DocServeMain {
+
+    public static void main(final String[] args) throws Exception {
+
+    }
+
+}
---
+++ doc-serve/src/main/java/docserve.java
@@ -0,0 +1,9 @@
+import com.namazustudios.socialengine.docserve.DocServeMain;
+
+public class docserve {
+
+    public static void main(final String args[]) throws Exception {
+        DocServeMain.main(args);
+    }
+
+}
