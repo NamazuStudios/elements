cb409579d81b565f1feddef3464b206c98ffcb2e
Patrick Twohig <patrick@namazustudios.com>
2021-07-15 08:42:39 -0700

    Updated the HappyServlet to return 404 on anything except the root path.

--- common-servlet/src/main/java/com/namazustudios/socialengine/servlet/security/HappyServlet.java
+++ common-servlet/src/main/java/com/namazustudios/socialengine/servlet/security/HappyServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static javax.servlet.http.HttpServletResponse.SC_NOT_FOUND;
 import static javax.servlet.http.HttpServletResponse.SC_OK;
 
 public class HappyServlet extends HttpServlet {
@@ -13,9 +14,15 @@ public class HappyServlet extends HttpServlet {
     @Override
     protected void doGet(final HttpServletRequest req,
                          final HttpServletResponse resp) throws IOException {
-        resp.setStatus(SC_OK);
-        resp.setContentType("text/plain");
-        resp.getOutputStream().write("OK".getBytes(UTF_8));
+        if ("/".equals(req.getServletPath())) {
+            resp.setStatus(SC_OK);
+            resp.setContentType("text/plain");
+            resp.getOutputStream().write("OK".getBytes(UTF_8));
+        } else {
+            resp.setStatus(SC_NOT_FOUND);
+            resp.setContentType("text/plain");
+            resp.getOutputStream().write("NOT FOUND".getBytes(UTF_8));
+        }
     }
 
 }
