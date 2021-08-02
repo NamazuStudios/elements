9d3e8e5f96b3e772fb4968477e03687bd0d74ed5
Patrick Twohig <patrick@namazustudios.com>
2021-08-02 15:03:41 -0700

    SOC-431: Added code to ensure Docker container starts with testing

--- mongo-dao/src/test/java/com/namazustudios/socialengine/dao/mongo/IntegrationTestModule.java
+++ mongo-dao/src/test/java/com/namazustudios/socialengine/dao/mongo/IntegrationTestModule.java
@@ -13,12 +13,16 @@ import org.slf4j.LoggerFactory;
 import ru.vyarus.guice.validator.ValidationModule;
 
 import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Properties;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 import static com.namazustudios.socialengine.dao.mongo.provider.MongoClientProvider.MONGO_CLIENT_URI;
 import static java.lang.String.format;
+import static java.lang.Thread.sleep;
 import static java.util.UUID.randomUUID;
 
 public class IntegrationTestModule extends AbstractModule {
@@ -26,6 +30,10 @@ public class IntegrationTestModule extends AbstractModule {
     private static final Logger logger = LoggerFactory.getLogger(IntegrationTestModule.class);
 
     private static final int TEST_MONGO_PORT = 45000;
+
+    private static final int CONNECT_POLLING_RATE = 1000;
+
+    private static final int CONNECT_POLLING_CYCLES = 300;
 
     private static final String TEST_MONGO_VERSION = "3.4.5";
 
@@ -52,6 +60,7 @@ public class IntegrationTestModule extends AbstractModule {
                     format("-p%d:27017", TEST_MONGO_PORT),
                     format("mongo:%s", TEST_MONGO_VERSION)
                 )
+                .redirectErrorStream(true)
                 .start();
 
             final var stdout = new Thread(log(process::getInputStream, m -> logger.info("mongod {}", m)));
@@ -62,10 +71,22 @@ public class IntegrationTestModule extends AbstractModule {
             stderr.setDaemon(true);
             stderr.start();
 
-            hooks.add(process::destroy);
-            hooks.add(() -> new ProcessBuilder().command(new String[] {"docker", "kill", uuid}).start());
+            hooks.add(() -> {
 
-        } catch (IOException e) {
+                logger.info("Destroying mongo process.");
+                process.destroy();
+
+                final var kill = new ProcessBuilder()
+                    .command("docker", "kill", uuid)
+                    .start();
+
+                kill.waitFor();
+
+            });
+
+            waitForConnect();
+
+        } catch (IOException | InterruptedException e) {
             addError(e);
             return;
         }
@@ -95,16 +116,36 @@ public class IntegrationTestModule extends AbstractModule {
 
     }
 
+    private void waitForConnect() throws InterruptedException, UnknownHostException {
+
+        final var addr = InetAddress.getByAddress(new byte[]{127,0,0,1});
+
+        for (int i = 0; i < CONNECT_POLLING_CYCLES; ++i) {
+            try (final var socket = new Socket(addr, TEST_MONGO_PORT)) {
+                break;
+            } catch (IOException e) {
+                sleep(CONNECT_POLLING_RATE);
+            }
+        }
+
+    }
+
     public Runnable log(final Supplier<InputStream> inputStreamSupplier,
                         final Consumer<String> messageConsumer) {
         return () -> {
             try (var r = new InputStreamReader(inputStreamSupplier.get());
                  var br = new BufferedReader(r)) {
-                final var line = br.readLine();
-                messageConsumer.accept(line);
+
+                var line = br.readLine();
+
+                while (line != null) {
+                    messageConsumer.accept(line);
+                    line = br.readLine();
+                }
+
             } catch (EOFException ex) {
-                logger.trace("Hit end of stream.");
-            }catch (IOException ex) {
+                logger.info("Hit end of stream.");
+            } catch (IOException ex) {
                 logger.info("Caught IO Exception reading subprocess.", ex);
             }
         };
