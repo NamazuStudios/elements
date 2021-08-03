cb9174de07770aa3d5253798c859be50d7c8a9e2
Patrick Twohig <patrick@namazustudios.com>
2021-08-02 17:10:35 -0700

    SOC-431: Removed delete all from tests for items

--- mongo-dao/src/test/java/com/namazustudios/socialengine/dao/mongo/MongoItemDaoIntegrationTest.java
+++ mongo-dao/src/test/java/com/namazustudios/socialengine/dao/mongo/MongoItemDaoIntegrationTest.java
@@ -17,6 +17,7 @@ import javax.inject.Inject;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import static java.lang.String.format;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
 
@@ -31,21 +32,24 @@ public class MongoItemDaoIntegrationTest {
 
     @Test
     public void testCreateAndRead() {
-        Item item = createMockItem();
-        Item createdItem = itemDao.createItem(item);
+
+        final var item = createMockItem("create_and_read");
+        final var createdItem = itemDao.createItem(item);
         assertNotNull(createdItem.getId());
 
-        Item byName = itemDao.getItemByIdOrName(createdItem.getName());
+        final var byName = itemDao.getItemByIdOrName(createdItem.getName());
         assertEquals(byName, createdItem);
-        Item byId = itemDao.getItemByIdOrName(createdItem.getId());
+
+        final var byId = itemDao.getItemByIdOrName(createdItem.getId());
         assertEquals(byId, createdItem);
+
     }
 
     @Test
     public void testCreateAndNonStringMetadataValue() {
-        Item item = createMockItem();
-        Item createdItem = itemDao.createItem(item);
-        assertEquals(createdItem.getMetadata().get("hamCount"), Integer.valueOf(2000000));
+        final var item = createMockItem("create_and_read_non_string_metadata");
+        final var createdItem = itemDao.createItem(item);
+        assertEquals(createdItem.getMetadata().get("hamCount"), 2000000);
     }
 
     @Test(expectedExceptions = NotFoundException.class)
@@ -60,28 +64,30 @@ public class MongoItemDaoIntegrationTest {
 
     @Test(expectedExceptions = DuplicateException.class, enabled = false)
     public void testCreateNameAlreadyExists() {
-        Item item = createMockItem();
-        Item created = itemDao.createItem(item);
+        final var item = createMockItem("create_name_already_exists");
+        final var created = itemDao.createItem(item);
         assertNotNull(created);
         itemDao.createItem(item);
     }
 
     @Test
     public void testModify() {
-        Item item = createMockItem();
-        Item createdItem = itemDao.createItem(item);
+
+        final var item = createMockItem("modify");
+        final var createdItem = itemDao.createItem(item);
         createdItem.setDisplayName("Different Display Name");
 
-        Item fromUpdate = itemDao.updateItem(createdItem);
+        final var fromUpdate = itemDao.updateItem(createdItem);
         assertEquals(fromUpdate.getDisplayName(), "Different Display Name");
 
-        Item lookedUpItem = itemDao.getItemByIdOrName(createdItem.getId());
+        final var lookedUpItem = itemDao.getItemByIdOrName(createdItem.getId());
         assertEquals(fromUpdate, lookedUpItem);
+
     }
 
     @Test(expectedExceptions = NotFoundException.class)
     public void testModifyNotFound() {
-        Item item = createMockItem();
+        final var item = createMockItem("modify_not_found");
         item.setId(new ObjectId().toHexString());
         itemDao.updateItem(item);
     }
@@ -94,7 +100,7 @@ public class MongoItemDaoIntegrationTest {
         //Add Items to collection, and track the objectIds that contain the tags we care about.
         Set<ObjectId> expectedObjectIds = new HashSet<>();
         for (int i = 0; i < numberOfItems; i++) {
-            Item item = createMockItem();
+            Item item = createMockItem(format("items_by_tags_%d", i));
             List<String> tagsForItem = new ArrayList<>();
             item.setName(RandomStringUtils.randomAlphabetic(30));
             if (i % 2 == 0) {
@@ -125,13 +131,14 @@ public class MongoItemDaoIntegrationTest {
         assertEquals(seen, expectedObjectIds);
     }
 
-    private Item createMockItem() {
+    private Item createMockItem(final String name) {
         Map<String, Object> mockMetadata = new HashMap<>();
+
         mockMetadata.put("ham", "eggs");
         mockMetadata.put("hamCount", 2000000);  // test non-string insertion/retrieval
 
         Item item = new Item();
-        item.setName("foo_item");
+        item.setName(name);
         item.setDisplayName("Foo");
         item.setMetadata(mockMetadata);
         item.setDescription("A Fooable Item.  Don't foo me bro");
@@ -151,23 +158,23 @@ public class MongoItemDaoIntegrationTest {
         return new Object[][] {
             {
                 "Case: displayName and description are trimmed",
-                createItem("name", " displayName\t ", "\tdescription", Lists.newArrayList(), new HashMap<>()),
-                createItem("name", "displayName", "description", Lists.newArrayList(), new HashMap<>()),
+                createItem("test_normalize_0", " displayName\t ", "\tdescription", Lists.newArrayList(), new HashMap<>()),
+                createItem("test_normalize_0", "displayName", "description", Lists.newArrayList(), new HashMap<>()),
             },
             {
                 "Case: tags are trimmed",
-                createItem("name", " displayName ", "\tdescription", Lists.newArrayList(" foo ", "\tbar\t"), new HashMap<>()),
-                createItem("name", "displayName", "description", Lists.newArrayList("foo","bar"), new HashMap<>()),
+                createItem("test_normalize_1", " displayName ", "\tdescription", Lists.newArrayList(" foo ", "\tbar\t"), new HashMap<>()),
+                createItem("test_normalize_1", "displayName", "description", Lists.newArrayList("foo","bar"), new HashMap<>()),
             },
             {
                 "Case: tags have inner non alpha-numeric characters, including whitespace, replaced with _",
-                createItem("name", " displayName ", "\tdescription", Lists.newArrayList("foo!bar ", "\r\r\t"), new HashMap<>()),
-                createItem("name", "displayName", "description", Lists.newArrayList("foo!bar"), new HashMap<>()),
+                createItem("test_normalize_2", " displayName ", "\tdescription", Lists.newArrayList("foo!bar ", "\r\r\t"), new HashMap<>()),
+                createItem("test_normalize_2", "displayName", "description", Lists.newArrayList("foo!bar"), new HashMap<>()),
             },
             {
                 "Case: tags have upper case characters made lower case",
-                createItem("name", " displayName ", "\tdescription", Lists.newArrayList("b\r\r\n\tig   ", "B   ang"), new HashMap<>()),
-                createItem("name", "displayName", "description", Lists.newArrayList("big","B_ang"), new HashMap<>()),
+                createItem("test_normalize_3", " displayName ", "\tdescription", Lists.newArrayList("b\r\r\n\tig   ", "B   ang"), new HashMap<>()),
+                createItem("test_normalize_3", "displayName", "description", Lists.newArrayList("big","B_ang"), new HashMap<>()),
             }
         };
     }
@@ -206,13 +213,4 @@ public class MongoItemDaoIntegrationTest {
         this.matchingMockObjects = matchingMockObjects;
     }
 
-    @BeforeMethod
-    public void deleteAllItems() {
-        Datastore.find(MongoItem.class).delete(new DeleteOptions().multi(true));
-    }
-
-    @AfterSuite
-    public void killProcess() {
-//        getEmbeddedMongo().stop();
-    }
 }
