03c023f72e1b5fae7705bc912a758ed33dc5202c
Robert Cornwall <>
2021-11-15 11:19:59 -0800

    SOC-457 Auth scheme create and update request classes

---
+++ common/src/main/java/com/namazustudios/socialengine/model/auth/UpdateAuthSchemeRequest.java
@@ -0,0 +1,7 @@
+package com.namazustudios.socialengine.model.auth;
+
+import io.swagger.annotations.ApiModel;
+
+@ApiModel(description = "Represents a request to update an Auth Scheme for an Application.")
+public class UpdateAuthSchemeRequest {
+}
