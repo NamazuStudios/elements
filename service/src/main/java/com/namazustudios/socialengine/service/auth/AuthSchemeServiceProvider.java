2a24d906c2d004ceef0af39effc71ae63693890e
Robert Cornwall <>
2021-11-16 11:29:26 -0800

    SOC-457 Created AuthSchemeServiceProvider

---
+++ service/src/main/java/com/namazustudios/socialengine/service/auth/AuthSchemeServiceProvider.java
@@ -0,0 +1,10 @@
+package com.namazustudios.socialengine.service.auth;
+
+import javax.inject.Provider;
+
+public class AuthSchemeServiceProvider implements Provider<AuthSchemeService> {
+    @Override
+    public AuthSchemeService get() {
+        return null;
+    }
+}
