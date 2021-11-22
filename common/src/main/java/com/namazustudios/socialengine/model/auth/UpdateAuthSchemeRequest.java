69ed84d5500854a8a697966bcdcff94a41fb7a9b
Robert Cornwall <>
2021-11-22 12:22:44 -0800

    SOC-457 Getters/settings for auth scheme request

--- common/src/main/java/com/namazustudios/socialengine/model/auth/UpdateAuthSchemeRequest.java
+++ common/src/main/java/com/namazustudios/socialengine/model/auth/UpdateAuthSchemeRequest.java
@@ -9,6 +9,10 @@ import java.util.List;
 @ApiModel(description = "Represents a request to update an Auth Scheme for an Application.")
 public class UpdateAuthSchemeRequest {
 
+    @ApiModelProperty("The unique id of the auth scheme.")
+    @NotNull
+    public String authSchemeId;
+
     @ApiModelProperty("The JWT audience for the scheme. Must be unique.")
     @NotNull
     public String aud;
@@ -27,4 +31,48 @@ public class UpdateAuthSchemeRequest {
     @ApiModelProperty("The list of issuers allowed to use this scheme.")
     @NotNull
     public List<String> allowedIssuers;
+
+    public String getAuthSchemeId() {
+        return authSchemeId;
+    }
+
+    public void setAuthSchemeId(String authSchemeId) {
+        this.authSchemeId = authSchemeId;
+    }
+
+    public String getAud() {
+        return aud;
+    }
+
+    public void setAud(String aud) {
+        this.aud = aud;
+    }
+
+    public Boolean getRegenerate() { return regenerate; }
+
+    public void setRegenerate(Boolean regenerate) { this.regenerate = regenerate; }
+
+    public String getPubKey() {
+        return pubKey;
+    }
+
+    public void setPubKey(String pubKey) {
+        this.pubKey = pubKey;
+    }
+
+    public String getUserLevel() {
+        return userLevel;
+    }
+
+    public void setUserLevel(String userLevel) {
+        this.userLevel = userLevel;
+    }
+
+    public List<String> getAllowedIssuers() {
+        return allowedIssuers;
+    }
+
+    public void setAllowedIssuers(List<String> allowedIssuers) {
+        this.allowedIssuers = allowedIssuers;
+    }
 }
