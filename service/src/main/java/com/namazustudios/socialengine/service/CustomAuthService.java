f8d491b2b8e956746b38619e90b57a78545b4301
Robert Cornwall <>
2021-12-22 13:36:00 -0800

    SOC-461 Added custom auth service

---
+++ service/src/main/java/com/namazustudios/socialengine/service/CustomAuthService.java
@@ -0,0 +1,20 @@
+package com.namazustudios.socialengine.service;
+
+import com.namazustudios.socialengine.rt.annotation.Expose;
+import com.namazustudios.socialengine.rt.annotation.ExposedBindingAnnotation;
+import com.namazustudios.socialengine.rt.annotation.ExposedModuleDefinition;
+
+/**
+ * Used to authorize requests using JWT auth tokens
+ *
+ * Created by robb on 12/20/21.
+ */
+@Expose({
+        @ExposedModuleDefinition(value = "namazu.elements.service.custom.auth"),
+        @ExposedModuleDefinition(
+                value = "namazu.elements.service.auth.unscoped.authscheme",
+                annotation = @ExposedBindingAnnotation(Unscoped.class)
+        )})
+public interface CustomAuthService {
+
+}
