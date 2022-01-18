6e6c929327f1cc7ec50016f5e8b80b5d8aa59009
Garrett McSpadden <EmissaryEntertainment@aol.com>
2022-01-18 10:33:34 -0800

    SOC-476 Added ThreadLocal instance for Neow3j object. SOC-476 Created single instances of the ScheduledExecutorService and HttpService for use with the Neow3j object

--- service/src/main/java/com/namazustudios/socialengine/service/blockchain/StandardNeow3jClient.java
+++ service/src/main/java/com/namazustudios/socialengine/service/blockchain/StandardNeow3jClient.java
@@ -1,18 +1,19 @@
 package com.namazustudios.socialengine.service.blockchain;
 
 import com.namazustudios.socialengine.Constants;
-import com.namazustudios.socialengine.exception.blockchain.Neow3jClientException;
 import com.namazustudios.socialengine.model.blockchain.neo.*;
 import io.neow3j.contract.SmartContract;
 import io.neow3j.crypto.exceptions.CipherException;
 import io.neow3j.crypto.exceptions.NEP2InvalidFormat;
 import io.neow3j.crypto.exceptions.NEP2InvalidPassphrase;
 import io.neow3j.protocol.Neow3j;
+import io.neow3j.protocol.Neow3jConfig;
 import io.neow3j.protocol.http.HttpService;
 import io.neow3j.script.ScriptBuilder;
 import io.neow3j.transaction.TransactionBuilder;
 import io.neow3j.types.ContractParameter;
 import io.neow3j.types.Hash160;
+import io.neow3j.utils.Async;
 import io.neow3j.wallet.Account;
 import io.neow3j.wallet.Wallet;
 import io.neow3j.wallet.nep6.NEP6Account;
@@ -22,11 +23,11 @@ import io.neow3j.wallet.nep6.NEP6Wallet;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import java.io.IOException;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ScheduledExecutorService;
 
 import static java.lang.String.format;
 import static java.util.stream.Collectors.toList;
@@ -34,42 +35,17 @@ import static java.util.stream.Collectors.toMap;
 
 public class StandardNeow3jClient implements Neow3jClient {
 
-    private Neow3j neow3j;
+    private HttpService httpService;
 
-    private String neoHost;
+    private final ScheduledExecutorService executorService = Async.defaultExecutorService();
 
-    private String neoPort;
+    private final Neow3jConfig neow3jConfig = new Neow3jConfig().setScheduledExecutorService(executorService);
+
+    private final ThreadLocal<Neow3j> neow3jThreadLocal = ThreadLocal.withInitial(() -> Neow3j.build(httpService, neow3jConfig));
 
     @Override
     public Neow3j getNeow3j() {
-        if (neow3j == null) {
-            neow3j = Neow3j.build(new HttpService(format("%s:%s", getNeoHost(), getNeoPort())));
-        }
-        try {
-            var response = neow3j.getConnectionCount().send();
-            if (response.hasError()) {
-                neow3j = Neow3j.build(new HttpService(format("%s:%s", getNeoHost(), getNeoPort())));
-            }
-            response = neow3j.getConnectionCount().send();
-            if (response.hasError()) {
-                throw new Neow3jClientException("Neow3j encountered an error " + response.getError());
-            }
-        } catch (IOException e) { // This will catch if we are not connected, so we will attempt to reconnect and try again before throwing an exception.
-            neow3j = Neow3j.build(new HttpService(format("%s:%s", getNeoHost(), getNeoPort())));
-            try {
-                var response = neow3j.getConnectionCount().send();
-                if (response.hasError()) {
-                    neow3j = Neow3j.build(new HttpService(format("%s:%s", getNeoHost(), getNeoPort())));
-                }
-                response = neow3j.getConnectionCount().send();
-                if (response.hasError()) {
-                    throw new Neow3jClientException("Neow3j encountered and error " + response.getError());
-                }
-            } catch (IOException ex) {
-                throw new Neow3jClientException("Neow3j client exception: " + ex);
-            }
-        }
-        return neow3j;
+        return neow3jThreadLocal.get();
     }
 
     @Override
@@ -197,21 +173,9 @@ public class StandardNeow3jClient implements Neow3jClient {
                 .collect(toMap(e -> convertObject(e.getKey()), e -> convertObject(e.getValue())));
     }
 
-    private String getNeoHost() {
-        return neoHost;
-    }
-
     @Inject
-    private void setNeoHost(@Named(Constants.NEO_BLOCKCHAIN_HOST)String neoHost) {
-        this.neoHost = neoHost;
-    }
-
-    private String getNeoPort() {
-        return neoPort;
-    }
-
-    @Inject
-    private void setNeoPort(@Named(Constants.NEO_BLOCKCHAIN_PORT)String neoPort) {
-        this.neoPort = neoPort;
+    private void setHttpService(@Named(Constants.NEO_BLOCKCHAIN_HOST)String neoHost,
+                                @Named(Constants.NEO_BLOCKCHAIN_PORT)String neoPort) {
+        httpService = new HttpService(format("%s:%s", neoHost, neoPort));
     }
 }
