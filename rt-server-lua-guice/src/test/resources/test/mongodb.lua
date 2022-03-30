eee3c3614c909d858ce6855a15368e48c6672af5
Keith Hudnall <keith@namazustudios.com>
2022-03-30 10:21:28 -0700

    SOC-502 Added lua resources

---
+++ rt-server-lua-guice/src/test/resources/test/mongodb.lua
@@ -0,0 +1,98 @@
+---
+--- Created by keithhudnall.
+--- DateTime: 3/22/22 3:32 PM
+---
+
+local mongodb = require "namazu.mongodb"
+
+local mongodb_test = {}
+
+
+function mongodb_test.test_get_elements_database()
+
+    local success, error = mongodb.elements_database()
+
+    --Return error if fail or nil if success
+    return success == nil and error or nil
+end
+
+
+function mongodb_test.test_get_application_collection()
+
+    local success, error = mongodb.application_collection()
+
+    --Return error if fail or nil if success
+    return success == nil and error or nil
+end
+
+
+function mongodb_test.test_get_users_collection()
+
+    local db = mongodb.elements_database()
+    local success, error = db:getCollection("user")
+
+    --Return error if fail or nil if success
+    return success == nil and error or nil
+end
+
+
+function mongodb_test.test_create_application_entries()
+
+    local c = mongodb.application_collection()
+
+    local entry_one = {
+        ["_id"] = "1",
+        ["name"] = "entry_one"
+    }
+
+    local entry_two = {
+        ["_id"] = "2",
+        ["name"] = "entry_two"
+    }
+
+    --Returns true. On error, returns nil and the error message.
+    local success, error = c:insertMany(entry_one, entry_two)
+
+    --Return error if fail or nil if success
+    return success == nil and error or nil
+end
+
+
+function mongodb_test.test_modify_application_entries()
+
+    local c = mongodb.application_collection()
+    local query = '{"_id":"1"}'
+    local new_name = "new_name"
+    local success, error = c:update(query, {name = new_name})
+
+    if(success) then
+
+        success, error = c:findOne(query)
+
+        if(success) then
+
+            local entry = success:value()
+
+            if(entry.name == new_name) then
+                return nil
+            end
+
+            return "Update not successful!"
+        end
+    end
+
+    return error
+end
+
+
+function mongodb_test.test_delete_application_entries()
+
+    local c = mongodb.application_collection()
+
+    local success, error = c:removeMany('')
+
+    --Return error if fail or nil if success
+    return success == nil and error or nil
+end
+
+return mongodb_test
---
+++ rt-server-lua/src/main/resources/namazu/mongodb.lua
@@ -0,0 +1,37 @@
+---
+--- Created by keithhudnall.
+--- DateTime: 3/22/22 11:34 AM
+---
+
+local ioc = require "namazu.ioc.resolver"
+local connect_uri = ioc:inject("java.lang.String", "com.namazustudios.socialengine.mongo.uri")
+local database_name = ioc:inject("java.lang.String", "com.namazustudios.socialengine.mongo.database.name")
+local application_id = ioc:resolve("com.namazustudios.socialengine.rt.id.ApplicationId"):asString()
+local mongo = require "mongo"
+
+
+local mongodb = {
+    client = mongo.Client(connect_uri)
+}
+
+
+function mongodb.elements_database()
+
+    return mongodb.client:getDatabase(database_name)
+end
+
+
+function mongodb.application_collection()
+
+    local db = mongodb.elements_database()
+
+    if(db:hasCollection(application_id)) then
+
+        return db:getCollection()
+    end
+
+    return db:createCollection(application_id)
+end
+
+
+return mongodb
