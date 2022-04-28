afcdd44586c26bf192cce30c66771a24a0580691
Keith Hudnall <keith@namazustudios.com>
2022-04-28 14:28:22 -0700

    SOC-502 rolled back last changes

--- mongo-dao/src/test/java/com/namazustudios/socialengine/dao/mongo/IntegrationTestModule.java
+++ mongo-dao/src/test/java/com/namazustudios/socialengine/dao/mongo/IntegrationTestModule.java
@@ -2,24 +2,18 @@ package com.namazustudios.socialengine.dao.mongo;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.name.Names;
-import com.namazustudios.socialengine.MongoTestModule;
-import com.namazustudios.socialengine.config.DefaultConfigurationSupplier;
+import com.namazustudios.socialengine.MongoTestInstanceModule;
 import com.namazustudios.socialengine.dao.mongo.guice.MongoCoreModule;
 import com.namazustudios.socialengine.dao.mongo.guice.MongoDaoModule;
 import com.namazustudios.socialengine.dao.mongo.guice.MongoSearchModule;
 import com.namazustudios.socialengine.dao.mongo.provider.MongoDozerMapperProvider;
-import com.namazustudios.socialengine.guice.ConfigurationModule;
+import com.namazustudios.socialengine.security.PasswordGenerator;
+import com.namazustudios.socialengine.security.SecureRandomPasswordGenerator;
 import dev.morphia.Datastore;
 import org.dozer.Mapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testng.annotations.Guice;
 import ru.vyarus.guice.validator.ValidationModule;
-
-import java.util.Properties;
-
-import static com.namazustudios.socialengine.dao.mongo.provider.MongoClientProvider.MONGO_CLIENT_URI;
-import static java.lang.String.format;
 
 
 public class IntegrationTestModule extends AbstractModule {
@@ -34,15 +28,6 @@ public class IntegrationTestModule extends AbstractModule {
 
     @Override
     protected void configure() {
-        
-        final DefaultConfigurationSupplier defaultConfigurationSupplier;
-        defaultConfigurationSupplier = new DefaultConfigurationSupplier();
-
-        install(new ConfigurationModule(() -> {
-            final Properties properties = defaultConfigurationSupplier.get();
-            properties.put(MONGO_CLIENT_URI, format("mongodb://%s:%d", TEST_BIND_IP, TEST_MONGO_PORT));
-            return properties;
-        }));
 
         install(new MongoDaoModule(){
             @Override
@@ -56,11 +41,15 @@ public class IntegrationTestModule extends AbstractModule {
             .annotatedWith(Names.named(TEST_COMPONENT))
             .toProvider(MongoDozerMapperProvider.class);
 
+        bind(PasswordGenerator.class)
+                .to(SecureRandomPasswordGenerator.class)
+                .asEagerSingleton();
+
         bind(UserTestFactory.class).asEagerSingleton();
         bind(ProfileTestFactory.class).asEagerSingleton();
         bind(ApplicationTestFactory.class).asEagerSingleton();
 
-        install(new MongoTestModule());
+        install(new MongoTestInstanceModule());
         install(new MongoCoreModule());
         install(new MongoSearchModule());
         install(new ValidationModule());
