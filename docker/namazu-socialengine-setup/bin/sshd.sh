77d5b73a0e7bf98f6ae2d43b2edb8f6a238b52a7
Patrick Twohig <patrick@namazustudios.com>
2022-06-27 23:07:55 -0700

    SOC-510: Added environment variable support to the script for host keys.

--- docker/namazu-socialengine-setup/bin/sshd.sh
+++ docker/namazu-socialengine-setup/bin/sshd.sh
@@ -4,6 +4,34 @@ run_sshd=/run/sshd
 sshd_pid=/run/sshd.pid
 pam_env_conf=/etc/security/pam_env.conf
 
+if [[ -z "${ELEMENTS_CONF}" ]];
+then
+  echo "No file specified. Exiting."
+  exit 1
+fi
+
+ssh_host_rsa_key="${ELEMENTS_CONF}/ssh_host_rsa_key"
+ssh_host_dsa_key="${ELEMENTS_CONF}/ssh_host_dsa_key"
+ssh_host_ecdsa_key="${ELEMENTS_CONF}/ssh_host_ecdsa_key"
+authorized_keys="${ELEMENTS_CONF}/authorized_keys"
+
+function decode_base64_to_file() {
+
+  if [[ -n "${1}" ]];
+  then
+
+      if [[ -z "${2}" ]];
+      then
+        echo "No file specified. Exiting."
+        exit 1
+      fi
+
+    echo "${1}" | base64 --decode > "${2}"
+
+  fi
+
+}
+
 if [ -f "$sshd_pid" ]
 then
     kill "$(cat "$sshd_pid")" > /dev/null 2>&1
@@ -15,6 +43,11 @@ mkdir -p "$run_sshd"
 chown root:root "$run_sshd"
 chmod go-rwx "$run_sshd"
 
+decode_base64_to_file "${SECRET_ELEMENTS_SSH_HOST_RSA_KEY_BASE64}" "$ELEMENTS_CONF/ssh_host_rsa_key"
+decode_base64_to_file "${SECRET_ELEMENTS_SSH_HOST_DSA_KEY_BASE64}" "$ELEMENTS_CONF/ssh_host_dsa_key"
+decode_base64_to_file "${SECRET_ELEMENTS_SSH_HOST_ECDSA_KEY_BASE64}" "$ELEMENTS_CONF/ssh_host_ecdsa_key"
+decode_base64_to_file "${SECRET_ELEMENTS_SSH_AUTHORIZED_KEYS_BASE64}" "$ELEMENTS_CONF/authorized_keys"
+
 echo "CLASSPATH=$CLASSPATH" > "$pam_env_conf"
 env | grep "ELEMENTS_*" >> "$pam_env_conf"
 env | grep '^com[\._]namazustudios[\._].*' >> "$pam_env_conf"
@@ -23,7 +56,11 @@ echo "PAM Environment."
 cat "$pam_env_conf"
 
 chown root:root "$ELEMENTS_CONF/authorized_keys"
-chmod 644 "$ELEMENTS_CONF/authorized_keys"
+
+chmod 600 "${ssh_host_rsa_key}"
+chmod 600 "${ssh_host_dsa_key}"
+chmod 600 "${ssh_host_ecdsa_key}"
+chmod 644 "${authorized_keys}"
 
 /usr/sbin/sshd -D -e -f "$ELEMENTS_CONF/sshd_config"
 rm -f "$sshd_pid"
