db13676543716943e31d66875604d9419fa2f0af
Patrick Twohig <patrick@namazustudios.com>
2022-11-18 10:55:09 -0800

    SOC-561: Forbidden -> Unimplemented

--- service/src/main/java/com/namazustudios/socialengine/service/formidium/FormidiumServiceProvider.java
+++ service/src/main/java/com/namazustudios/socialengine/service/formidium/FormidiumServiceProvider.java
@@ -1,17 +1,14 @@
 package com.namazustudios.socialengine.service.formidium;
 
-import com.namazustudios.socialengine.service.Services;
-
 import javax.inject.Provider;
 
-import static com.namazustudios.socialengine.service.Services.forbidden;
+import static com.namazustudios.socialengine.service.Services.unimplemented;
 
 public class FormidiumServiceProvider implements Provider<FormidiumService> {
 
     @Override
     public FormidiumService get() {
-        // TODO Bind to Formidium Service
-        return forbidden(FormidiumService.class);
+        return unimplemented(FormidiumService.class);
     }
 
 }
