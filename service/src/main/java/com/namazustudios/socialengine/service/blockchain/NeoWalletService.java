fc46f7112823e769ab0bac1e5f8d9d22f2537be2
Patrick Twohig <patrick@namazustudios.com>
2022-12-15 12:32:02 -0800

    SOC-559: Fixed duplicate binding

--- service/src/main/java/com/namazustudios/socialengine/service/blockchain/NeoWalletService.java
+++ service/src/main/java/com/namazustudios/socialengine/service/blockchain/NeoWalletService.java
@@ -18,9 +18,9 @@ import java.util.List;
  * Created by keithhudnall on 9/22/21.
  */
 @Expose({
-    @ModuleDefinition(value = "namazu.elements.service.blockchain.wallet"),
+    @ModuleDefinition(value = "namazu.elements.service.blockchain.neo.wallet"),
     @ModuleDefinition(
-        value = "namazu.elements.service.blockchain.unscoped.wallet",
+        value = "namazu.elements.service.blockchain.unscoped.neo.wallet",
         annotation = @ExposedBindingAnnotation(Unscoped.class)
     )
 })
