d57d94f19767b2f478b4145f96e19f83e0705ac0
Patrick Twohig <patrick@namazustudios.com>
2022-12-19 14:49:44 -0800

    SOC-559: Fixed bad mongodb query

--- mongo-dao/src/main/java/com/namazustudios/socialengine/dao/mongo/blockchain/MongoWalletDao.java
+++ mongo-dao/src/main/java/com/namazustudios/socialengine/dao/mongo/blockchain/MongoWalletDao.java
@@ -63,7 +63,7 @@ public class MongoWalletDao implements WalletDao {
         }
 
         if (protocol != null)
-            query.filter(eq("protocol", protocol));
+            query.filter(eq("api", protocol));
 
         if (networks != null) {
             if (networks.isEmpty()) return Pagination.empty();
