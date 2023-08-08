14b56d3282332702195dd3d2f71b28ed1af636b7
Marek Serwanski <marek.serwanski@gmail.com>
2023-08-08 12:05:39 +0200

    attributes form init

--- web-ui-angular-jetty/elements-web-ui/src/app/application-dialog/application-dialog.component.ts
+++ web-ui-angular-jetty/elements-web-ui/src/app/application-dialog/application-dialog.component.ts
@@ -1,7 +1,7 @@
-import {Component, Inject, OnInit} from '@angular/core';
+import {Component, Inject, OnInit, ViewChild} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import {MatSnackBar} from '@angular/material/snack-bar';
-import {FormBuilder, Validators} from "@angular/forms";
+import {FormBuilder, Validators} from '@angular/forms';
 import {AlertService} from '../alert.service';
 
 @Component({
@@ -11,11 +11,21 @@ import {AlertService} from '../alert.service';
 })
 export class ApplicationDialogComponent implements OnInit {
 
+  showAdvanced = false;
+  public isJSONValid = true;
+
   applicationForm = this.formBuilder.group({
     name: [ this.data.application.name, [Validators.required, Validators.pattern('^[a-zA-Z0-9]+$') ]],
     description: [ this.data.application.description ],
     scriptRepoUrl: [ { value: this.data.application.scriptRepoUrl, disabled: true } ]
   });
+
+  toggleAdvancedEditor() {
+    if (this.showAdvanced) {
+      this.validateMetadata(true);
+    }
+    this.showAdvanced = !this.showAdvanced;
+  }
 
   constructor(public dialogRef: MatDialogRef<ApplicationDialogComponent>,
               @Inject(MAT_DIALOG_DATA) public data: any,
@@ -24,7 +34,7 @@ export class ApplicationDialogComponent implements OnInit {
   ngOnInit() {
     this.alertService.getMessage().subscribe((message: any) => {
       if(message) {
-        this.snackBar.open(message.text, "Dismiss", { duration: 3000 });
+        this.snackBar.open(message.text, 'Dismiss', { duration: 3000 });
       }
     });
   }
@@ -41,5 +51,20 @@ export class ApplicationDialogComponent implements OnInit {
     }, err => {
       this.alertService.error(err);
     });
+  }
+
+  // TODO: try unify with json-editor-card
+  validateMetadata(andUpdate: boolean) {
+    try {
+      // editor.get() throws error if JSON invalid
+      // const editorContents = this.editor.get();
+
+      // if (andUpdate) { this.editTarget.metadata = editorContents; }
+      this.isJSONValid = true;
+    } catch (err) {
+      // bad JSON detected...don't let them leave the advanced editor!
+      this.isJSONValid = false;
+      return;
+    }
   }
 }
