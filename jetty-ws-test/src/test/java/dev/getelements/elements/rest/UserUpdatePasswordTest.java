58e8a33692d86d9dbfcf7319ea10fdd312148ca1
ptwohig <patrick@namazustudios.com>
2023-11-01 20:17:21 -0700

    EL-29: Added a third test.

--- jetty-ws-test/src/test/java/dev/getelements/elements/rest/UserUpdatePasswordTest.java
+++ jetty-ws-test/src/test/java/dev/getelements/elements/rest/UserUpdatePasswordTest.java
@@ -22,6 +22,14 @@ import static org.testng.AssertJUnit.assertEquals;
 
 public class UserUpdatePasswordTest {
 
+    public static final String ORIGINAL_PASSWORD = ClientContext.DUMMY_PASSWORD;
+
+    public static final String FIRST_UPDATE = "first_password_update";
+
+    public static final String SECOND_UPDATE = "second_password_update";
+
+    public static final String ALWAYS_FAILS = "always_fails_every_time";
+
     @Factory
     public Object[] getTests() {
         return new Object[] {
@@ -45,8 +53,15 @@ public class UserUpdatePasswordTest {
 
     @BeforeClass
     public void setUp() {
-        user0.createUser("password-test").createSession();
-        user1.createUser("password-test").createSession();
+
+        user0.createUser("password-test")
+                .createProfile("password-test")
+                .createSession();
+
+        user1.createUser("password-test")
+                .createProfile("password-test")
+                .createSession();
+
     }
 
     @DataProvider
@@ -58,8 +73,8 @@ public class UserUpdatePasswordTest {
     public void testUpdatePassword(final ClientContext context) {
 
         final var request = new UserUpdatePasswordRequest();
-        request.setNewPassword("updatedpassword");
-        request.setOldPassword(ClientContext.DUMMY_PASSWORD);
+        request.setNewPassword(FIRST_UPDATE);
+        request.setOldPassword(ORIGINAL_PASSWORD);
 
         var response = client
                 .target(format("%s/user/%s/password", apiRoot, context.getUser().getId()))
@@ -86,6 +101,74 @@ public class UserUpdatePasswordTest {
 
         final var user = response.readEntity(User.class);
         assertEquals(context.getUser().getId(), user.getId());
+
+    }
+
+    @Test(dataProvider = "getContexts", dependsOnMethods = "testUpdatePassword")
+    public void testUpdatePasswordWithProfile(final ClientContext context) {
+
+        final var request = new UserUpdatePasswordRequest();
+        request.setNewPassword(SECOND_UPDATE);
+        request.setOldPassword(FIRST_UPDATE);
+        request.setProfileId(context.getDefaultProfile().getId());
+
+        context.createSession();
+
+        var response = client
+                .target(format("%s/user/%s/password", apiRoot, context.getUser().getId()))
+                .request()
+                .header("Authorization", format("Bearer %s", context.getSessionSecret()))
+                .put(Entity.entity(request, MediaType.APPLICATION_JSON_TYPE));
+
+        assertEquals(200, response.getStatus());
+
+        final var sessionCreation = response.readEntity(SessionCreation.class);
+        assertNotNull(sessionCreation.getSession());
+        assertNotNull(sessionCreation.getSessionSecret());
+
+        final var session = sessionCreation.getSession();
+        assertNotNull(session.getUser());
+        assertNotNull(session.getProfile());
+        assertNotNull(session.getApplication());
+
+        response = client
+                .target(format("%s/user/me", apiRoot))
+                .request()
+                .header("Authorization", format("Bearer %s", sessionCreation.getSessionSecret()))
+                .get();
+
+        final var user = response.readEntity(User.class);
+        assertEquals(context.getUser().getId(), user.getId());
+
+    }
+
+
+    @DataProvider
+    public Object[][] getCrossUserContexts() {
+        return new Object[][] {
+                {user0, user1},
+                {user1, user0}
+        };
+    }
+
+    @Test(dataProvider = "getCrossUserContexts", dependsOnMethods = "testUpdatePasswordWithProfile")
+    public void testUpdateCrossUserFails(final ClientContext authorized, final ClientContext unauthorized) {
+
+        final var request = new UserUpdatePasswordRequest();
+        request.setNewPassword(ALWAYS_FAILS);
+        request.setOldPassword(SECOND_UPDATE);
+        request.setProfileId(authorized.getDefaultProfile().getId());
+
+        authorized.createSession();
+        unauthorized.createSession();
+
+        var response = client
+                .target(format("%s/user/%s/password", apiRoot, authorized.getUser().getId()))
+                .request()
+                .header("Authorization", format("Bearer %s", unauthorized.getSessionSecret()))
+                .put(Entity.entity(request, MediaType.APPLICATION_JSON_TYPE));
+
+        assertEquals(404, response.getStatus());
 
     }
 
