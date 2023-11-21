f77b04cf7c3d31ba18fe0da35509fe1b5b03131e
PiotrMichałkiewicz <piotrek.michalkiewicz@gmail.com>
2023-11-21 19:13:56 +0000

    Merged in bugfix/EL-39-update-avatar-test-failure (pull request #18)
    Bugfix/EL-39 update avatar test failure
    
    * Errors resolved
    
    * Added id match assertion
    
    * Commented out currently redundant code
    
    * Uncommented updateLargeObjectConent method for test
    
    
    Approved-by: Marek

--- jetty-ws-test/src/test/java/dev/getelements/elements/rest/UpdateUserAvatarTest.java
+++ jetty-ws-test/src/test/java/dev/getelements/elements/rest/UpdateUserAvatarTest.java
@@ -45,28 +45,30 @@ public class UpdateUserAvatarTest {
         clientContext.createProfile("Test Display");
     }
 
-//    @Test
-//    public void shouldUpdateUserAvatar() {
-//
-//        clientContext.createSession(clientContext.getDefaultProfile());
-//
-//        String profileId = clientContext.getDefaultProfile().getId();
-//
+    @Test
+    public void shouldUpdateUserAvatar() {
+
+        clientContext.createSession(clientContext.getDefaultProfile());
+
+        String profileId = clientContext.getDefaultProfile().getId();
+        String largeObjectIdBeforeUpdate = clientContext.getDefaultProfile().getImageObject().getId();
+
 //        updateLargeObjectContent(clientContext.getDefaultProfile().getImageObject().getId());
-//
-//        UpdateProfileImageRequest updateProfileImageRequest = new UpdateProfileImageRequest();
-//        updateProfileImageRequest.setMimeType("image/jpeg");
-//
-//        Profile updatedProfile = client
-//                .target(apiRoot + "/profile/" + profileId + "/image")
-//                .request()
-//                .header(SESSION_SECRET, clientContext.getSessionSecret())
-//                .put(Entity.entity(updateProfileImageRequest, MediaType.APPLICATION_JSON_TYPE))
-//                .readEntity(Profile.class);
-//
-//        assertNotNull(updatedProfile.getImageObject().getId());
-//        assertEquals(updatedProfile.getImageObject().getMimeType(), "image/jpeg");
-//    }
+
+        UpdateProfileImageRequest updateProfileImageRequest = new UpdateProfileImageRequest();
+        updateProfileImageRequest.setMimeType("image/jpeg");
+
+        Profile updatedProfile = client
+                .target(apiRoot + "/profile/" + profileId + "/image")
+                .request()
+                .header(SESSION_SECRET, clientContext.getSessionSecret())
+                .put(Entity.entity(updateProfileImageRequest, MediaType.APPLICATION_JSON_TYPE))
+                .readEntity(Profile.class);
+
+        assertNotNull(updatedProfile.getImageObject().getId());
+        assertEquals(updatedProfile.getImageObject().getId(), largeObjectIdBeforeUpdate);
+        assertEquals(updatedProfile.getImageObject().getMimeType(), "image/jpeg");
+    }
 
     private void updateLargeObjectContent(String largeObjectId) {
         final InputStream loStream = UserLargeObjectResourceTest.class.getResourceAsStream("/testLO.txt");
