9ab2033707717f8722d93790c99d3f66f63c0e65
Marek Serwanski <marek.serwanski@gmail.com>
2023-12-29 18:22:12 +0100

    EL-32 invite via phoneNb list logic

---
+++ service/src/main/java/dev/getelements/elements/service/InviteService.java
@@ -0,0 +1,32 @@
+package dev.getelements.elements.service;
+
+import dev.getelements.elements.model.invite.InviteViaPhonesRequest;
+import dev.getelements.elements.model.invite.InviteViaPhonesResponse;
+import dev.getelements.elements.rt.annotation.DeprecationDefinition;
+import dev.getelements.elements.rt.annotation.Expose;
+import dev.getelements.elements.rt.annotation.ModuleDefinition;
+
+@Expose({
+        @ModuleDefinition(
+                value = "eci.elements.service.invite"
+        ),
+        @ModuleDefinition(
+                value = "namazu.elements.service.invite",
+                deprecated = @DeprecationDefinition("Use eci.elements.service.invite instead.")
+        )
+})
+public interface InviteService {
+
+    int PROFILES_PER_USER_LIMIT = 5;
+    int USERS_PER_PHONE_LIMIT = 5;
+    String USER_BY_PHONE_QUERY = "primaryPhoneNb:%s";
+
+    /**
+     * Get an {@link InviteViaPhonesResponse} by invite request data with phone number list
+     *
+     * @param inviteRequest the request
+     * @return the {@link InviteViaPhonesResponse}
+     */
+    InviteViaPhonesResponse inviteViaPhoneNumbers(InviteViaPhonesRequest inviteRequest, int offset, int count);
+
+}
