368f762f085d15119c1e47705bd8472fea3b1a8f
Patrick Twohig <patrick@namazustudios.com>
2025-03-14 21:53:21 -0700

    EL-88: UPdated Test Jars

--- sdk-test/src/main/java/dev/getelements/elements/sdk/test/ElementLoaderTest.java
+++ sdk-test/src/main/java/dev/getelements/elements/sdk/test/ElementLoaderTest.java
@@ -34,11 +34,11 @@ public class ElementLoaderTest {
                         "dev.getelements.elements.sdk.test.element.a",
                         VARIANT_A
                 )
-//                ,
-//                new ElementLoaderTest(
-//                        "dev.getelements.elements.sdk.test.element.b",
-//                        VARIANT_B
-//                )
+                ,
+                new ElementLoaderTest(
+                        "dev.getelements.elements.sdk.test.element.b",
+                        VARIANT_B
+                )
         };
     }
 
@@ -121,11 +121,10 @@ public class ElementLoaderTest {
         elementRegistry.publish(event1);
         elementRegistry.publish(event2);
 
-// TODO: Fix with EL-88
-//Just the startup event
         Assert.assertEquals(testService.getConsumedEvents().size(), 1);
         Assert.assertEquals(testService.getConsumedEvents().getFirst().getEventName(), ElementLoader.SYSTEM_EVENT_ELEMENT_LOADED);
         Assert.assertEquals(testService.getConsumedEventObjects().size(), 2);
+
         Assert.assertEquals(testService.getConsumedEventObjects().get(0).name(), TEST_ELEMENT_EVENT_1);
         Assert.assertEquals(testService.getConsumedEventObjects().get(0).arguments().get(0), eventObject1);
         Assert.assertEquals(testService.getConsumedEventObjects().get(0).arguments().get(1), eventObject1);
