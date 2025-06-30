f494fe66be7ae03068d9a5c256142b26c7f2a854
ptwohig <patrick@namazustudios.com>
2025-06-29 17:20:55 -0700

    EL-102: Removed unnecsary annotation and upated the SPI for Guice

--- sdk-guice/src/main/java/dev/getelements/elements/sdk/guice/package-info.java
+++ sdk-guice/src/main/java/dev/getelements/elements/sdk/guice/package-info.java
@@ -2,7 +2,4 @@
  * The Guice SDK module is intended for using Guice to build an {@link dev.getelements.elements.sdk.Element} based
  * system. This is used internally in the core Elements product.
  */
-@ElementPublic
 package dev.getelements.elements.sdk.guice;
-
-import dev.getelements.elements.sdk.annotation.ElementPublic;
