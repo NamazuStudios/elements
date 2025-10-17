e0a8266aa10940d92b5b85aeebf695926d0b4a83
ptwohig <patrick@namazustudios.com>
2025-10-17 14:16:22 -0700

    EL-266: Fixed failing test.

--- service-test/src/test/java/dev/getelements/elements/service/TestServicesModule.java
+++ service-test/src/test/java/dev/getelements/elements/service/TestServicesModule.java
@@ -1,8 +1,13 @@
 package dev.getelements.elements.service;
 
 import dev.getelements.elements.cdnserve.guice.FileSystemCdnGitLoaderModule;
+import dev.getelements.elements.common.app.ApplicationDeploymentService;
 import dev.getelements.elements.sdk.guice.SharedElementModule;
 import dev.getelements.elements.service.guice.ServicesModule;
+import org.mockito.Mockito;
+
+import static com.google.inject.name.Names.named;
+import static dev.getelements.elements.common.app.ApplicationDeploymentService.APP_SERVE;
 
 public class TestServicesModule extends SharedElementModule {
 
@@ -14,6 +19,10 @@ public class TestServicesModule extends SharedElementModule {
     protected void configureElement() {
         install(new FileSystemCdnGitLoaderModule());
         install(new ServicesModule(TestScope.scope));
+
+        bind(ApplicationDeploymentService.class)
+                .annotatedWith(named(APP_SERVE))
+                .toInstance(Mockito.mock(ApplicationDeploymentService.class));
     }
 
 }
