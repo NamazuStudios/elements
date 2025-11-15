4dbb82565bfd5702ed198498cec98ef845c3fba5
ptwohig <patrick@namazustudios.com>
2025-11-15 15:14:22 -0800

    EL-284: Added fix for the reflection exception in the element report screen.

--- sdk-mongo/src/main/java/dev/getelements/elements/sdk/mongo/package-info.java
+++ sdk-mongo/src/main/java/dev/getelements/elements/sdk/mongo/package-info.java
@@ -1,9 +1,6 @@
 @ElementPublic
-@ElementDefinition
-@GuiceElementModule(MongoSdkModule.class)
+@ElementDependency("dev.getelements.elements.sdk.mongo")
 package dev.getelements.elements.sdk.mongo;
 
-import dev.getelements.elements.sdk.annotation.ElementDefinition;
+import dev.getelements.elements.sdk.annotation.ElementDependency;
 import dev.getelements.elements.sdk.annotation.ElementPublic;
-import dev.getelements.elements.sdk.mongo.guice.MongoSdkModule;
-import dev.getelements.elements.sdk.spi.guice.annotations.GuiceElementModule;
