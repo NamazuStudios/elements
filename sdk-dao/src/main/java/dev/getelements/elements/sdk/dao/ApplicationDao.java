df545d12878ca7f453ca1e4c18b9545c4cec1544
Keith Hudnall <712020:96da829f-4ae0-4637-8dde-5a0ffc1910dd@bots.bitbucket.org>
2026-01-29 15:19:30 -0800

    EL-314 Changed application path to be more consistent with others. Added @Deprecated where appropriate, including renaming all methods referencing active or inactive applications.

--- sdk-dao/src/main/java/dev/getelements/elements/sdk/dao/ApplicationDao.java
+++ sdk-dao/src/main/java/dev/getelements/elements/sdk/dao/ApplicationDao.java
@@ -19,39 +19,39 @@ public interface ApplicationDao {
      * Creates a new application and inserts it into the database.  The returned value
      * represents the {@link Application} as it was inserted into the database.
      * <p>
-     * If the existing {@link Application} objects is int eh database however is flagged
+     * If the existing {@link Application} object is in the database and is flagged
      * as hidden or inactive this will simply reinstate the old record.
      *
      * @param application the application
      * @return the application instance as it was created
      */
-    Application createOrUpdateInactiveApplication(final Application application);
+    Application createApplication(final Application application);
 
     /**
      * Gets all active applications.
      *
      * @return a {@link Pagination<Application>} of all active {@link Application} instances
      */
-    Pagination<Application> getActiveApplications();
+    Pagination<Application> getApplications();
 
     /**
-     * Gets the active applications registered in the databse given the offset and count.
+     * Gets the active applications registered in the database given the offset and count.
      *
      * @param offset the offset
      * @param count  the count
      * @return a {@link Pagination} of {@link Application} instances
      */
-    Pagination<Application> getActiveApplications(final int offset, final int count);
+    Pagination<Application> getApplications(final int offset, final int count);
 
     /**
-     * Gets the active applications registered in the databse given the offset and count.
+     * Gets the active applications registered in the database given the offset and count.
      *
      * @param offset the offset
      * @param count  the count
      * @param search a query to filter the results
      * @return a {@link Pagination} of {@link Application} instances
      */
-    Pagination<Application> getActiveApplications(final int offset, final int count, final String search);
+    Pagination<Application> getApplications(final int offset, final int count, final String search);
 
     /**
      * Gets an application with the specific name or identifier.  This will throw an instance
@@ -60,6 +60,119 @@ public interface ApplicationDao {
      * @return an Application instance, never null
      * @throws NotFoundException if the application is inactive or non-existent
      */
+    default Application getApplication(final String nameOrId) {
+        return findActiveApplication(nameOrId)
+                .orElseThrow(() -> new ApplicationNotFoundException("Application " + nameOrId + " not found."));
+    }
+
+    /**
+     * Finds an active {@link Application} with the supplied name or id.
+     *
+     * @param nameOrId the name or id
+     * @return an {@link Optional} of the {@link Application}
+     */
+    Optional<Application> findApplication(final String nameOrId);
+
+    /**
+     * Extension of getActiveApplication with no attributes set.
+     * of {@link NotFoundException} if the object cannot be found, or is inactive.
+     *
+     * @return an Application instance, never null
+     * @throws NotFoundException if the application is inactive or non-existent
+     */
+    Application getApplicationWithoutAttributes(final String nameOrId);
+
+    /**
+     * Updates an application with the specific name/id.  When changing
+     * the application name the ID must be specified.
+     *
+     * @param application the {@link Application} object to write
+     * @return the {@link Application} object as it was persisted to the database.
+     * @throws NotFoundException if the application does not exist.
+     */
+    Application updateApplication(final Application application);
+
+    /**
+     * Deletes an Application with the specific name or identifier.  The application is no
+     * actually removed, but rather it is flagged as inactive to preserve any
+     * consistency with dependent data.
+     *
+     * @param nameOrId the name, or id
+     */
+    void softDeleteApplication(final String nameOrId);
+
+    /**
+     * Updates an application with the specific name/identifiers.  When changing
+     * the application name the ID must be specified.
+     *
+     * @param nameOrId    the name, or id
+     * @param application the {@link Application} object to write
+     * @return the {@link Application} object as it was persisted to the database.
+     * @throws NotFoundException if the application does not exist.
+     */
+    @Deprecated
+    default Application updateActiveApplication(final String nameOrId, final Application application) {
+        return updateApplication(application);
+    }
+
+    /**
+     * Creates a new application and inserts it into the database.  The returned value
+     * represents the {@link Application} as it was inserted into the database.
+     * <p>
+     * If the existing {@link Application} objects is int eh database however is flagged
+     * as hidden or inactive this will simply reinstate the old record.
+     *
+     * @param application the application
+     * @return the application instance as it was created
+     */
+    @Deprecated
+    default Application createOrUpdateInactiveApplication(final Application application) {
+        return createApplication(application);
+    }
+
+    /**
+     * Gets all active applications.
+     *
+     * @return a {@link Pagination<Application>} of all active {@link Application} instances
+     */
+    @Deprecated
+    default Pagination<Application> getActiveApplications() {
+        return getApplications();
+    }
+
+    /**
+     * Gets the active applications registered in the database given the offset and count.
+     *
+     * @param offset the offset
+     * @param count  the count
+     * @return a {@link Pagination} of {@link Application} instances
+     */
+    @Deprecated
+    default Pagination<Application> getActiveApplications(final int offset, final int count) {
+        return getApplications(offset, count);
+    }
+
+    /**
+     * Gets the active applications registered in the database given the offset and count.
+     *
+     * @param offset the offset
+     * @param count  the count
+     * @param search a query to filter the results
+     * @return a {@link Pagination} of {@link Application} instances
+     */
+    @Deprecated
+    default Pagination<Application> getActiveApplications(final int offset, final int count, final String search) {
+        return getActiveApplications(offset, count);
+    }
+
+    /**
+     * Gets an application with the specific name or identifier.  This will throw an instance
+     * of {@link NotFoundException} if the object cannot be found, or is inactive.
+     *
+     * @return an Application instance, never null
+     * @throws NotFoundException if the application is inactive or non-existent
+     */
+    @Deprecated
     default Application getActiveApplication(final String nameOrId) {
         return findActiveApplication(nameOrId)
                 .orElseThrow(() -> new ApplicationNotFoundException("Application " + nameOrId + " not found."));
@@ -71,7 +184,10 @@ public interface ApplicationDao {
      * @param nameOrId the name or id
      * @return an {@link Optional} of the {@link Application}
      */
-    Optional<Application> findActiveApplication(final String nameOrId);
+    @Deprecated
+    default Optional<Application> findActiveApplication(final String nameOrId) {
+        return findActiveApplication(nameOrId);
+    }
 
     /**
      * Extension of getActiveApplication with no attributes set.
@@ -80,26 +196,9 @@ public interface ApplicationDao {
      * @return an Application instance, never null
      * @throws NotFoundException if the application is inactive or non-existent
      */
-    Application getActiveApplicationWithoutAttributes(final String nameOrId);
-
-    /**
-     * Updates an application with the specific name/identifiers.  When changing
-     * the application name the ID must be specified.
-     *
-     * @param nameOrId    the name, or id
-     * @param application the {@link Application} object to write
-     * @return the {@link Application} object as it was persisted to the database.
-     * @throws NotFoundException if the application does not exist.
-     */
-    Application updateActiveApplication(final String nameOrId, final Application application);
-
-    /**
-     * Deletes an Application with the specific name or identifier.  The application is no
-     * actually removed, but rather it is flagged as inactive to preserve any
-     * consistency with dependent data.
-     *
-     * @param nameOrId the name, or id
-     */
-    void softDeleteApplication(final String nameOrId);
+    @Deprecated
+    default Application getActiveApplicationWithoutAttributes(final String nameOrId) {
+        return getApplicationWithoutAttributes(nameOrId);
+    }
 
 }
