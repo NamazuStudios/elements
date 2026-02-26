6f30e3d39cf6a6fe4a85d9ba137e54c22a4e13c6
Patrick Twohig <patrick@namazustudios.com>
2026-02-25 17:12:50 -0800

    EL-317: Added to archeypte.

--- sdk-element-standard/src/main/resources/archetype-resources/debug/src/main/java/run.java
+++ sdk-element-standard/src/main/resources/archetype-resources/debug/src/main/java/run.java
@@ -9,6 +9,12 @@ import dev.getelements.elements.sdk.local.ElementsLocalBuilder;
 public class run {
     public static void main(final String[] args ) {
 
+        new ProcessBuilder("docker", "compose", "up", "-d")
+                .directory(new java.io.File("services-dev"))
+                .inheritIO()
+                .start()
+                .waitFor();
+
         final var local = ElementsLocalBuilder.getDefault()
                 .withSourceRoot()
                 .withDeployment(builder -> builder
