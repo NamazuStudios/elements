6db3f6532957e6233f5ce2f19af3fe52c21fb13c
Patrick Twohig <patrick@namazustudios.com>
2026-02-25 21:30:37 -0800

    EL-317: Fixed exception loading Element.

--- sdk-spi-guice/src/main/java/dev/getelements/elements/sdk/spi/guice/GuiceSpiModule.java
+++ sdk-spi-guice/src/main/java/dev/getelements/elements/sdk/spi/guice/GuiceSpiModule.java
@@ -39,10 +39,9 @@ public class GuiceSpiModule extends PrivateModule {
         final var attributes = elementRecord.attributes().asProperties();
         final var iterator = attributes.entrySet().iterator();
 
-        for (var entry = iterator.next(); entry != null; entry = iterator.next()) {
-            if (entry.getValue() == null) {
-                iterator.remove();
-            }
+        while (!iterator.hasNext()) {
+            final var entry = iterator.next();
+            if (entry.getValue() == null) iterator.remove();
         }
 
         binder().requireExplicitBindings();
