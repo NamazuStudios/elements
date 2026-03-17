8977d2ea592c107a384525db4e00f7ac4c920b77
Patrick Twohig <patrick@namazustudios.com>
2026-03-16 20:25:29 -0700

    EL-333: Fixed <p> again.

--- sdk-mongo/src/main/java/dev/getelements/elements/sdk/mongo/MongoConfigurationService.java
+++ sdk-mongo/src/main/java/dev/getelements/elements/sdk/mongo/MongoConfigurationService.java
@@ -15,7 +15,7 @@ import java.util.function.Function;
  * deployment-time configuration. Using this service guarantees that any new {@link MongoClient}
  * instances created from the resulting configuration will connect properly to the database.</p>
  *
- * <p></p>The {@link MongoConfiguration} returned here represents the lowest and most direct level of database access.
+ * <p>The {@link MongoConfiguration} returned here represents the lowest and most direct level of database access.
  * In contrast, the services {@link MongoClient} and {@link MongoDatabase} services returned from this Element share
  * with the core DAO layer, which carry their own opinions on how to interact with the database (e.g. codec registries,
  * session handling). Use this service when you need a raw, unconfigured connection to MongoDB without those
