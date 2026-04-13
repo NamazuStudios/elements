345916b9a798f086c0e4d9bcbc536a1b40a4e1b2
Keith Hudnall <712020:96da829f-4ae0-4637-8dde-5a0ffc1910dd@bots.bitbucket.org>
2026-04-13 15:37:40 -0700

    EL-355 Fixed Guice issue

--- service/src/main/java/dev/getelements/elements/service/codegen/OpenApiCodegenServiceProvider.java
+++ service/src/main/java/dev/getelements/elements/service/codegen/OpenApiCodegenServiceProvider.java
@@ -1,6 +1,5 @@
 package dev.getelements.elements.service.codegen;
 
-import dev.getelements.elements.sdk.model.exception.ForbiddenException;
 import dev.getelements.elements.sdk.model.user.User;
 import dev.getelements.elements.sdk.service.codegen.CodegenService;
 import jakarta.inject.Inject;
@@ -10,16 +9,17 @@ public class OpenApiCodegenServiceProvider implements Provider<CodegenService> {
 
     private User user;
 
+    private Provider<AnonCodegenService> anonCodegenServiceProvider;
+
     private Provider<SuperUserOpenApiCodegenService> superUserOpenApiCodegenServiceProvider;
 
     @Override
     public CodegenService get() {
-
         switch (user.getLevel()) {
             case SUPERUSER:
                 return superUserOpenApiCodegenServiceProvider.get();
             default:
-                throw new ForbiddenException("You are not authorized to perform this operation");
+                return anonCodegenServiceProvider.get();
         }
     }
 
@@ -30,6 +30,15 @@ public class OpenApiCodegenServiceProvider implements Provider<CodegenService> {
     @Inject
     public void setUser(User user) {
         this.user = user;
+    }
+
+    public Provider<AnonCodegenService> getAnonCodegenServiceProvider() {
+        return anonCodegenServiceProvider;
+    }
+
+    @Inject
+    public void setAnonCodegenServiceProvider(Provider<AnonCodegenService> anonCodegenServiceProvider) {
+        this.anonCodegenServiceProvider = anonCodegenServiceProvider;
     }
 
     public Provider<SuperUserOpenApiCodegenService> getSuperUserOpenApiCodegenServiceProvider() {
