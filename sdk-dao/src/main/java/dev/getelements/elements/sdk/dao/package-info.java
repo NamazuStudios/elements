780ea53946442f819d3bf2d00cc03abc61108b0a
Keith Hudnall <712020:96da829f-4ae0-4637-8dde-5a0ffc1910dd@bots.bitbucket.org>
2026-04-23 15:51:42 -0700

    EL-365 Made morphia accessible via the mongo dao ElementDependency

--- sdk-dao/src/main/java/dev/getelements/elements/sdk/dao/package-info.java
+++ sdk-dao/src/main/java/dev/getelements/elements/sdk/dao/package-info.java
@@ -1,8 +1,11 @@
 @ElementPublic
 @ElementDefinition(recursive = true)
 @ElementDependency("dev.getelements.elements.sdk.mongo")
+@ElementService(Datastore.class)
 package dev.getelements.elements.sdk.dao;
 
 import dev.getelements.elements.sdk.annotation.ElementDefinition;
 import dev.getelements.elements.sdk.annotation.ElementDependency;
 import dev.getelements.elements.sdk.annotation.ElementPublic;
+import dev.getelements.elements.sdk.annotation.ElementService;
+import dev.morphia.Datastore;
